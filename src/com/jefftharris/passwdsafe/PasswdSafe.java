bcc55317c1b55da1eb6e25881538054914f0116b
Jeff Harris <jefftharris@gmail.com>
2009-12-14 17:42:13 -0500

    Load file in background
    Show progress dialog while file is loading.

--- src/com/jefftharris/passwdsafe/PasswdSafe.java
+++ src/com/jefftharris/passwdsafe/PasswdSafe.java
@@ -20,6 +20,8 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -39,13 +41,13 @@ public class PasswdSafe extends ExpandableListActivity {
 
     public static final String INTENT = "com.jefftharris.passwdsafe.action.VIEW";
 
-    private PwsFile itsPwsFile;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Log.i(TAG, "onCreate bundle:" + savedInstanceState + ", intent:" +
+              getIntent());
         showDialog(DIALOG_GET_PASSWD);
     }
 
@@ -101,90 +103,49 @@ public class PasswdSafe extends ExpandableListActivity {
         Intent intent = getIntent();
         showDialog(DIALOG_LOADING);
 
-        try {
-            // TODO: on pause, close file, clear password, etc.
-            // TODO: Remember last file used somewhere?
-            Log.i(TAG, "before load file");
-            itsPwsFile =
-                PwsFileFactory.loadFile(intent.getData().getPath(),
-                                        new StringBuilder(passwd));
-            Log.i(TAG, "after load file");
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                dismissDialog(DIALOG_LOADING);
+                if (msg.what == LoadFileThread.RESULT_DATA) {
+                    LoadFileData data = (LoadFileData)msg.obj;
+                    ExpandableListAdapter adapter =
+                        new SimpleExpandableListAdapter(PasswdSafe.this,
+                                                        data.itsGroupData,
+                                                        android.R.layout.simple_expandable_list_item_1,
+                                                        new String[] { GROUP },
+                                                        new int[] { android.R.id.text1 },
+                                                        data.itsChildData,
+                                                        android.R.layout.simple_expandable_list_item_1,
+                                                        new String[] { TITLE },
+                                                        new int[] { android.R.id.text1 });
+                    setListAdapter(adapter);
+                    Log.i(TAG, "adapter set");
 
-            ArrayList<Map<String, String>> groupData =
-                new ArrayList<Map<String, String>>();
-            ArrayList<ArrayList<HashMap<String, Object>>> childData =
-                new ArrayList<ArrayList<HashMap<String, Object>>>();
-
-            TreeMap<String, ArrayList<PwsRecord>> recsByGroup =
-                new TreeMap<String, ArrayList<PwsRecord>>();
-            Iterator<PwsRecord> recIter = itsPwsFile.getRecords();
-            while (recIter.hasNext()) {
-                PwsRecord rec = recIter.next();
-                String group = getGroup(rec);
-                ArrayList<PwsRecord> groupList = recsByGroup.get(group);
-                if (groupList == null) {
-                    groupList = new ArrayList<PwsRecord>();
-                    recsByGroup.put(group, groupList);
+                } else {
+                    Exception e = (Exception)msg.obj;
+                    new AlertDialog.Builder(PasswdSafe.this)
+                    .setMessage(e.toString())
+                    .setCancelable(false)
+                    .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            PasswdSafe.this.finish();
+                        }
+                    })
+                    .show();
                 }
-                groupList.add(rec);
             }
-            Log.i(TAG, "groups sorted");
+        };
 
-            RecordMapComparator comp = new RecordMapComparator();
-            for (Map.Entry<String, ArrayList<PwsRecord>> entry :
-                    recsByGroup.entrySet()) {
-                Log.i(TAG, "process group:" + entry.getKey());
-                Map<String, String> groupInfo =
-                    Collections.singletonMap(GROUP, entry.getKey());
-                groupData.add(groupInfo);
-
-                ArrayList<HashMap<String, Object>> children =
-                    new ArrayList<HashMap<String, Object>>();
-                for (PwsRecord rec : entry.getValue()) {
-                    HashMap<String, Object> recInfo = new HashMap<String, Object>();
-                    recInfo.put(TITLE, getTitle(rec));
-                    recInfo.put(RECORD, rec);
-                    children.add(recInfo);
-                }
-
-                Collections.sort(children, comp);
-                childData.add(children);
-            }
-            Log.i(TAG, "adapter data created");
-
-            ExpandableListAdapter adapter =
-                new SimpleExpandableListAdapter(this,
-                                                groupData,
-                                                android.R.layout.simple_expandable_list_item_1,
-                                                new String[] { GROUP },
-                                                new int[] { android.R.id.text1 },
-                                                childData,
-                                                android.R.layout.simple_expandable_list_item_1,
-                                                new String[] { TITLE },
-                                                new int[] { android.R.id.text1 });
-            setListAdapter(adapter);
-            Log.i(TAG, "adapter set");
-
-
-        } catch (Exception e) {
-            Log.e(TAG, "Exception", e);
-            new AlertDialog.Builder(this)
-            .setMessage(e.toString())
-            .setCancelable(false)
-            .setPositiveButton("Close", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    PasswdSafe.this.finish();
-                }
-            })
-            .show();
-        } finally {
-            dismissDialog(DIALOG_LOADING);
-        }
+        LoadFileThread thr = new LoadFileThread(intent.getData().getPath(),
+                                                new StringBuilder(passwd),
+                                                handler);
+        thr.start();
     }
 
-    private final String getGroup(PwsRecord rec)
+    private static final String getGroup(PwsRecord rec, PwsFile file)
     {
-        switch (itsPwsFile.getFileVersionMajor()) {
+        switch (file.getFileVersionMajor()) {
             case PwsFileV3.VERSION:
                 return rec.getField(PwsRecordV3.GROUP).toString();
             default:
@@ -192,13 +153,114 @@ public class PasswdSafe extends ExpandableListActivity {
         }
     }
 
-    private final String getTitle(PwsRecord rec)
+    private static final String getTitle(PwsRecord rec, PwsFile file)
     {
-        switch (itsPwsFile.getFileVersionMajor()) {
+        switch (file.getFileVersionMajor()) {
             case PwsFileV3.VERSION:
                 return rec.getField(PwsRecordV3.TITLE).toString();
             default:
                 return "TODO";
+        }
+    }
+
+
+    private static final class LoadFileData
+    {
+        public final ArrayList<Map<String, String>> itsGroupData;
+        public final ArrayList<ArrayList<HashMap<String, Object>>> itsChildData;
+
+        public LoadFileData(ArrayList<Map<String, String>> groupData,
+                            ArrayList<ArrayList<HashMap<String, Object>>> childData)
+        {
+            itsGroupData = groupData;
+            itsChildData = childData;
+        }
+    }
+
+    private static final class LoadFileThread extends Thread
+    {
+        private final String itsFile;
+        private StringBuilder itsPasswd;
+        private final Handler itsMsgHandler;
+
+        public static final int RESULT_DATA = 0;
+        public static final int RESULT_EXCEPTION = 1;
+
+        public LoadFileThread(String file,
+                              StringBuilder passwd,
+                              Handler msgHandler) {
+            itsFile = file;
+            itsPasswd = passwd;
+            itsMsgHandler = msgHandler;
+        }
+
+        @Override
+        public void run() {
+            LoadFileData data = null;
+            Exception resultException = null;
+            try {
+                // TODO: on pause, close file, clear password, etc.
+                // TODO: Remember last file used somewhere?
+                Log.i(TAG, "before load file");
+                PwsFile pwsfile = PwsFileFactory.loadFile(itsFile, itsPasswd);
+                itsPasswd = null;
+                Log.i(TAG, "after load file");
+
+                ArrayList<Map<String, String>> groupData =
+                    new ArrayList<Map<String, String>>();
+                ArrayList<ArrayList<HashMap<String, Object>>> childData =
+                    new ArrayList<ArrayList<HashMap<String, Object>>>();
+
+                TreeMap<String, ArrayList<PwsRecord>> recsByGroup =
+                    new TreeMap<String, ArrayList<PwsRecord>>();
+                Iterator<PwsRecord> recIter = pwsfile.getRecords();
+                while (recIter.hasNext()) {
+                    PwsRecord rec = recIter.next();
+                    String group = getGroup(rec, pwsfile);
+                    ArrayList<PwsRecord> groupList = recsByGroup.get(group);
+                    if (groupList == null) {
+                        groupList = new ArrayList<PwsRecord>();
+                        recsByGroup.put(group, groupList);
+                    }
+                    groupList.add(rec);
+                }
+                Log.i(TAG, "groups sorted");
+
+                RecordMapComparator comp = new RecordMapComparator();
+                for (Map.Entry<String, ArrayList<PwsRecord>> entry :
+                        recsByGroup.entrySet()) {
+                    Log.i(TAG, "process group:" + entry.getKey());
+                    Map<String, String> groupInfo =
+                        Collections.singletonMap(GROUP, entry.getKey());
+                    groupData.add(groupInfo);
+
+                    ArrayList<HashMap<String, Object>> children =
+                        new ArrayList<HashMap<String, Object>>();
+                    for (PwsRecord rec : entry.getValue()) {
+                        HashMap<String, Object> recInfo = new HashMap<String, Object>();
+                        recInfo.put(TITLE, getTitle(rec, pwsfile));
+                        recInfo.put(RECORD, rec);
+                        children.add(recInfo);
+                    }
+
+                    Collections.sort(children, comp);
+                    childData.add(children);
+                }
+                Log.i(TAG, "adapter data created");
+                data = new LoadFileData(groupData, childData);
+            } catch (Exception e) {
+                Log.e(TAG, "Exception", e);
+                resultException = e;
+            }
+
+            Message msg;
+            if (data != null) {
+                msg = Message.obtain(itsMsgHandler, RESULT_DATA, data);
+            } else {
+                msg = Message.obtain(itsMsgHandler, RESULT_EXCEPTION,
+                                     resultException);
+            }
+            itsMsgHandler.sendMessage(msg);
         }
     }
 
