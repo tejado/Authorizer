14ae5772934ec9feff7fd0adf0d4876bf23348a4
Jeff Harris <jefftharris@gmail.com>
2009-12-31 15:54:45 -0500

    Add copyrights to files

--- src/com/jefftharris/passwdsafe/BCProvider.java
+++ src/com/jefftharris/passwdsafe/BCProvider.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (©) 2009-2010 Jeff Harris <jeffharris@users.sourceforge.net>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 package com.jefftharris.passwdsafe;
 
 import java.security.Provider;
