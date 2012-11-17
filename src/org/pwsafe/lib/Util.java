bff1c521c31a4554ba5657365401263015a68aa5
Jeff Harris <jefftharris@gmail.com>
2012-11-17 09:23:13 -0500

    Fix lint warning about insecure seeding of SecureRandom

--- src/org/pwsafe/lib/Util.java
+++ src/org/pwsafe/lib/Util.java
@@ -26,9 +26,6 @@ public final class Util
 	private static final char	HEX_CHARS[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
 	private static final SecureRandom randGen = new SecureRandom();
-	static {
-		randGen.setSeed(System.currentTimeMillis());
-	}
 
 	/**
 	 * Private to prevent instantiation.
