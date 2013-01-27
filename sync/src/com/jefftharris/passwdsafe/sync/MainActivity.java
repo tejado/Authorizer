efe459e1621cdedd652ba1a8df94bd15f3ebcc6f
Jeff Harris <jefftharris@gmail.com>
2013-01-27 13:50:58 -0500

    Format and add copyright

--- sync/src/com/jefftharris/passwdsafe/sync/MainActivity.java
+++ sync/src/com/jefftharris/passwdsafe/sync/MainActivity.java
@@ -1,22 +1,31 @@
+/*
+ * Copyright (©) 2013 Jeff Harris <jefftharris@gmail.com> All rights reserved.
+ * Use of the code is allowed under the Artistic License 2.0 terms, as specified
+ * in the LICENSE file distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 package com.jefftharris.passwdsafe.sync;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
 
 }
