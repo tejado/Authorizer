1cb7542dfc5cc88e661e2968968dbc199eb60eca
Jeff Harris <jefftharris@gmail.com>
2013-10-06 14:31:39 -0400

    Convert the IME to use an android keyboard view

--- passwdsafe/src/com/jefftharris/passwdsafe/PasswdSafeIME.java
+++ passwdsafe/src/com/jefftharris/passwdsafe/PasswdSafeIME.java
@@ -9,8 +9,15 @@ package com.jefftharris.passwdsafe;
 
 import org.pwsafe.lib.file.PwsRecord;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.XmlResourceParser;
 import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
@@ -29,7 +36,19 @@ import com.jefftharris.passwdsafe.util.Pair;
  */
 public class PasswdSafeIME extends InputMethodService
 {
+    private static final int USER_KEY = -1;
+    private static final int URL_KEY = -2;
+    private static final int BACK_KEY = -3;
+    private static final int PASSWORD_KEY = -11;
+    private static final int EMAIL_KEY = -12;
+    private static final int ENTER_KEY = -13;
+    private static final int TITLE_KEY = -21;
+    private static final int NOTES_KEY = -22;
+
     private View itsView;
+    private KeyboardView itsKeyboardView;
+    private Keyboard itsKeyboard;
+    private Keyboard.Key itsEnterKey;
     private int itsEnterAction = EditorInfo.IME_ACTION_NONE;
 
     /* (non-Javadoc)
@@ -51,31 +70,11 @@ public class PasswdSafeIME extends InputMethodService
             }
         });
 
-        OnClickListener fieldListener = new OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                sendText(v.getId());
-            }
-        };
-        for (int id: new int[] {
-                R.id.user, R.id.password, R.id.url, R.id.email,
-                R.id.title, R.id.notes}) {
-            btn = (Button)itsView.findViewById(id);
-            btn.setOnClickListener(fieldListener);
-        }
-
-        OnClickListener commandListener = new OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                doBtnCommand(v.getId());
-            }
-        };
-        for (int id: new int[] {R.id.backspace, R.id.return_key, R.id.next}) {
-            btn = (Button)itsView.findViewById(id);
-            btn.setOnClickListener(commandListener);
-        }
+        itsKeyboard = new PasswdSafeKeyboard(this, R.xml.keyboard);
+        itsKeyboardView = (KeyboardView)itsView.findViewById(R.id.keyboard);
+        itsKeyboardView.setPreviewEnabled(false);
+        itsKeyboardView.setKeyboard(itsKeyboard);
+        itsKeyboardView.setOnKeyboardActionListener(new KeyboardListener());
 
         return itsView;
     }
@@ -120,13 +119,12 @@ public class PasswdSafeIME extends InputMethodService
             break;
         }
         }
-        Button btn = (Button)itsView.findViewById(R.id.return_key);
-        btn.setText(enterText);
 
-        // TODO: remove next if doesn't work well
-        btn = (Button)itsView.findViewById(R.id.next);
-        btn.setVisibility((itsEnterAction != EditorInfo.IME_ACTION_NEXT) ?
-                View.VISIBLE : View.GONE);
+        if (itsEnterKey != null) {
+            itsEnterKey.label = getString(enterText);
+        }
+        // Reset keyboard to reflect key changes
+        itsKeyboardView.setKeyboard(itsKeyboard);
     }
 
     /* (non-Javadoc)
@@ -158,8 +156,8 @@ public class PasswdSafeIME extends InputMethodService
         }
     }
 
-    /** Send text to the connected editor */
-    private final void sendText(int btnId)
+    /** Handle a press of a keyboard key */
+    private final void onKeyPress(int keycode)
     {
         InputConnection conn = getCurrentInputConnection();
         if (conn == null) {
@@ -167,65 +165,67 @@ public class PasswdSafeIME extends InputMethodService
         }
 
         String str = null;
-        Pair<PasswdFileData, PwsRecord> rc = refresh();
-        if (rc.second != null) {
-            switch (btnId) {
-            case R.id.user: {
+        switch (keycode) {
+        case USER_KEY:
+        case PASSWORD_KEY:
+        case TITLE_KEY:
+        case URL_KEY:
+        case EMAIL_KEY:
+        case NOTES_KEY: {
+            Pair<PasswdFileData, PwsRecord> rc = refresh();
+            if (rc.second == null) {
+                break;
+            }
+            switch (keycode) {
+            case USER_KEY: {
                 str = rc.first.getUsername(rc.second);
                 break;
             }
-            case R.id.password: {
+            case PASSWORD_KEY: {
                 str = rc.first.getPassword(rc.second);
                 break;
             }
-            case R.id.url: {
-                str = rc.first.getURL(rc.second);
-                break;
-            }
-            case R.id.email: {
-                str = rc.first.getEmail(rc.second);
-                break;
-            }
-            case R.id.title: {
+            case TITLE_KEY: {
                 str = rc.first.getTitle(rc.second);
                 break;
             }
-            case R.id.notes: {
+            case URL_KEY: {
+                str = rc.first.getURL(rc.second);
+                break;
+            }
+            case EMAIL_KEY: {
+                str = rc.first.getEmail(rc.second);
+                break;
+            }
+            case NOTES_KEY: {
                 str = rc.first.getNotes(rc.second);
                 break;
             }
             }
+            break;
         }
-        if (str != null) {
-            conn.commitText(str, 1);
-        }
-    }
-
-    /** Do a button command */
-    private final void doBtnCommand(int btnId)
-    {
-        InputConnection conn = getCurrentInputConnection();
-        if (conn == null) {
-            return;
-        }
-
-        switch (btnId) {
-        case R.id.backspace: {
+        case BACK_KEY: {
             conn.deleteSurroundingText(1, 0);
             break;
         }
-        case R.id.return_key: {
+        case ENTER_KEY: {
             if (itsEnterAction == EditorInfo.IME_ACTION_NONE) {
-                conn.commitText("\n", 1);
+                conn.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN,
+                                               KeyEvent.KEYCODE_ENTER));
+                conn.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP,
+                                               KeyEvent.KEYCODE_ENTER));
             } else {
                 conn.performEditorAction(itsEnterAction);
             }
             break;
         }
-        case R.id.next: {
-            conn.performEditorAction(EditorInfo.IME_ACTION_NEXT);
+        case 32: {
+            str = " ";
             break;
         }
+        }
+        if (str != null) {
+            conn.commitText(str, 1);
         }
     }
 
@@ -236,6 +236,7 @@ public class PasswdSafeIME extends InputMethodService
         // TODO: Check field type for password pastes?
         // TODO: show group
         // TODO: disable blank fields?
+        // TODO: icons?
 
         PasswdSafeApp app = getPasswdSafeApp();
         PasswdFileData fileData = app.accessOpenFileData();
@@ -266,5 +267,101 @@ public class PasswdSafeIME extends InputMethodService
     private final PasswdSafeApp getPasswdSafeApp()
     {
         return (PasswdSafeApp)getApplication();
+    }
+
+    /** The PasswdSafeKeyboard class is a keyboard for PasswdSafe */
+    private final class PasswdSafeKeyboard extends Keyboard
+    {
+        /** Constructor */
+        public PasswdSafeKeyboard(Context context, int xmlLayoutResId)
+        {
+            super(context, xmlLayoutResId);
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.Keyboard#createKeyFromXml(android.content.res.Resources, android.inputmethodservice.Keyboard.Row, int, int, android.content.res.XmlResourceParser)
+         */
+        @Override
+        protected Key createKeyFromXml(Resources res,
+                                       Row parent,
+                                       int x,
+                                       int y,
+                                       XmlResourceParser parser)
+        {
+            Key key = super.createKeyFromXml(res, parent, x, y, parser);
+            if (key.codes[0] == ENTER_KEY) {
+                itsEnterKey = key;
+            }
+            return key;
+        }
+    }
+
+    /** The listener for keyboard events */
+    private final class KeyboardListener implements OnKeyboardActionListener
+    {
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onKey(int, int[])
+         */
+        @Override
+        public void onKey(int primaryCode, int[] keyCodes)
+        {
+            onKeyPress(primaryCode);
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onPress(int)
+         */
+        @Override
+        public void onPress(int primaryCode)
+        {
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onRelease(int)
+         */
+        @Override
+        public void onRelease(int primaryCode)
+        {
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#onText(java.lang.CharSequence)
+         */
+        @Override
+        public void onText(CharSequence text)
+        {
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeDown()
+         */
+        @Override
+        public void swipeDown()
+        {
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeLeft()
+         */
+        @Override
+        public void swipeLeft()
+        {
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeRight()
+         */
+        @Override
+        public void swipeRight()
+        {
+        }
+
+        /* (non-Javadoc)
+         * @see android.inputmethodservice.KeyboardView.OnKeyboardActionListener#swipeUp()
+         */
+        @Override
+        public void swipeUp()
+        {
+        }
     }
 }
