871fc079ebd2a90c90ef91879d6a069820456b6a
Jeff Harris <jefftharris@gmail.com>
2014-03-16 16:50:37 -0400

    Add initial layout to PasswdSafeListFragment

--- passwdsafe/src/com/jefftharris/passwdsafe/PasswdSafeListFragment.java
+++ passwdsafe/src/com/jefftharris/passwdsafe/PasswdSafeListFragment.java
@@ -7,11 +7,58 @@
  */
 package com.jefftharris.passwdsafe;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  *  Fragment showing lists of items from a PasswdSafe file
  */
 public class PasswdSafeListFragment extends ListFragment
 {
+    /** Listener interface for owning activity */
+    public interface Listener
+    {
+    }
+
+
+    private Listener itsListener;
+
+
+    /** Create a new instance */
+    public static PasswdSafeListFragment newInstance()
+    {
+        PasswdSafeListFragment frag = new PasswdSafeListFragment();
+        Bundle args = new Bundle();
+        frag.setArguments(args);
+        return frag;
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onAttach(android.app.Activity)
+     */
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+        itsListener = (Listener)activity;
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View root = inflater.inflate(R.layout.fragment_passwdsafe_list,
+                                     container, false);
+        return root;
+    }
 }
