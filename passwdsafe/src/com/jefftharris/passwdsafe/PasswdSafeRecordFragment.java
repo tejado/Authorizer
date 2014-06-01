6de9f75c956e1fb9d14ce904310084a64104ab7c
Jeff Harris <jefftharris@gmail.com>
2014-06-01 16:12:43 -0400

    Add skeleton menu to record view fragment

--- passwdsafe/src/com/jefftharris/passwdsafe/PasswdSafeRecordFragment.java
+++ passwdsafe/src/com/jefftharris/passwdsafe/PasswdSafeRecordFragment.java
@@ -15,6 +15,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -37,6 +40,8 @@ public class PasswdSafeRecordFragment extends Fragment
         void updateLocationView(PasswdLocation location);
     }
 
+    // TODO: When viewing record, should menu_close item close file or view
+    // of record
 
     private PasswdLocation itsLocation;
     private Listener itsListener;
@@ -114,6 +119,18 @@ public class PasswdSafeRecordFragment extends Fragment
 
 
     /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onActivityCreated(android.os.Bundle)
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+
+    /* (non-Javadoc)
      * @see android.support.v4.app.Fragment#onResume()
      */
     @Override
@@ -122,4 +139,64 @@ public class PasswdSafeRecordFragment extends Fragment
         super.onResume();
         itsListener.updateLocationView(itsLocation);
     }
+
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onCreateOptionsMenu(android.view.Menu, android.view.MenuInflater)
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.fragment_passwdsafe_record, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+
+    /* (non-Javadoc)
+     * @see android.support.v4.app.Fragment#onOptionsItemSelected(android.view.MenuItem)
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+        case R.id.menu_edit: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_delete: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_toggle_password: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_copy_user: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_copy_password: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_copy_notes: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_toggle_monospace: {
+            // TODO: impl
+            break;
+        }
+        case R.id.menu_toggle_wrap_notes: {
+            // TODO: impl
+            break;
+        }
+        default: {
+            return super.onOptionsItemSelected(item);
+        }
+        }
+        return true;
+    }
+
+
 }
