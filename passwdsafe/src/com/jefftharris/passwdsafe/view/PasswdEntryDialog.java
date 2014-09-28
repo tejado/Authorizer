51daaeec056817158e659c077b29b56307c3a488
Jeff Harris <jefftharris@gmail.com>
2014-09-28 11:54:59 -0400

    Cleanup import

--- passwdsafe/src/com/jefftharris/passwdsafe/view/PasswdEntryDialog.java
+++ passwdsafe/src/com/jefftharris/passwdsafe/view/PasswdEntryDialog.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
