8b0b6afa5010e6f83bd7d6c3ec8612d7bbd956ce
Jeff Harris <jefftharris@gmail.com>
2015-01-10 14:37:08 -0500

    Refactor common code for a sync remove file operation

--- sync/src/com/jefftharris/passwdsafe/sync/gdrive/GDriveRmFileOper.java
+++ sync/src/com/jefftharris/passwdsafe/sync/gdrive/GDriveRmFileOper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (©) 2013-2014 Jeff Harris <jefftharris@gmail.com> All rights reserved.
+ * Copyright (©) 2013-2015 Jeff Harris <jefftharris@gmail.com> All rights reserved.
  * Use of the code is allowed under the Artistic License 2.0 terms, as specified
  * in the LICENSE file distributed with this code, or available from
  * http://www.opensource.org/licenses/artistic-license-2.0.php
@@ -9,77 +9,31 @@ package com.jefftharris.passwdsafe.sync.gdrive;
 import java.io.IOException;
 
 import android.content.Context;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
 
 import com.google.api.services.drive.Drive;
-import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
-import com.jefftharris.passwdsafe.sync.R;
+import com.jefftharris.passwdsafe.sync.lib.AbstractRmSyncOper;
 import com.jefftharris.passwdsafe.sync.lib.DbFile;
-import com.jefftharris.passwdsafe.sync.lib.SyncDb;
 
 /**
  * A Google Drive sync operation to remove a file
  */
-public class GDriveRmFileOper extends GDriveSyncOper
+public class GDriveRmFileOper extends AbstractRmSyncOper<Drive>
 {
     private static final String TAG = "GDriveRmFileOper";
-
-    private final boolean itsIsRmLocal;
-    private final boolean itsIsRmRemote;
 
     /** Constructor */
     public GDriveRmFileOper(DbFile file)
     {
-        super(file);
-
-        itsIsRmLocal = (itsFile.itsLocalFile != null);
-        itsIsRmRemote = (!itsFile.itsIsRemoteDeleted &&
-                (itsFile.itsRemoteId != null));
+        super(file, TAG);
     }
 
     /* (non-Javadoc)
-     * @see com.jefftharris.passwdsafe.sync.GDriveSyncOper#doOper()
+     * @see com.jefftharris.passwdsafe.sync.lib.AbstractRmSyncOper#doRemoteRemove(java.lang.Object, android.content.Context)
      */
     @Override
-    public void doOper(Drive drive, Context ctx) throws IOException
+    protected void doRemoteRemove(Drive providerClient, Context ctx)
+            throws IOException
     {
-        PasswdSafeUtil.dbginfo(TAG, "removeFile %s", itsFile);
-        if (itsIsRmLocal) {
-            ctx.deleteFile(itsFile.itsLocalFile);
-        }
-
-        if (itsIsRmRemote) {
-            drive.files().trash(itsFile.itsRemoteId).execute();
-        }
-    }
-
-    /* (non-Javadoc)
-     * @see com.jefftharris.passwdsafe.sync.GDriveSyncOper#doPostOperUpdate(android.database.sqlite.SQLiteDatabase)
-     */
-    @Override
-    public void doPostOperUpdate(SQLiteDatabase db, Context ctx)
-            throws IOException, SQLException
-    {
-        SyncDb.removeFile(itsFile.itsId, db);
-    }
-
-    /* (non-Javadoc)
-     * @see com.jefftharris.passwdsafe.sync.GDriveSyncOper#getDescription()
-     */
-    @Override
-    public String getDescription(Context ctx)
-    {
-        String name = (itsFile.itsLocalTitle != null) ?
-                itsFile.getLocalTitleAndFolder() :
-                itsFile.getRemoteTitleAndFolder();
-
-        if (itsIsRmLocal && !itsIsRmRemote) {
-            return ctx.getString(R.string.sync_oper_rmfile_local, name);
-        } else if (!itsIsRmLocal && itsIsRmRemote) {
-            return ctx.getString(R.string.sync_oper_rmfile_remote, name);
-        } else {
-            return ctx.getString(R.string.sync_oper_rmfile, name);
-        }
+        providerClient.files().trash(itsFile.itsRemoteId).execute();
     }
 }
