ee3b81a440a6a7a77023759b3e7fb7ff38ab5933
Jeff Harris <jefftharris@gmail.com>
2015-04-29 21:53:06 -0400

    Lint cleanup

--- lib/src/main/java/com/jefftharris/passwdsafe/lib/ReleaseNotesDialog.java
+++ lib/src/main/java/com/jefftharris/passwdsafe/lib/ReleaseNotesDialog.java
@@ -14,6 +14,7 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.text.Html;
@@ -50,7 +51,7 @@ public class ReleaseNotesDialog extends DialogFragment
      * @see android.support.v4.app.DialogFragment#onCreateDialog(android.os.Bundle)
      */
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState)
+    public @NonNull Dialog onCreateDialog(Bundle savedInstanceState)
     {
         Activity act = getActivity();
         String notes = getString(R.string.release_notes);
