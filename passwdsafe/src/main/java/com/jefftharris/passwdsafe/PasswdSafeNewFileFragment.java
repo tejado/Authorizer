d4e3362b66a261d53eaeb1d3d812992522c67fdd
Jeff Harris <jefftharris@users.noreply.github.com>
2015-09-05 22:04:04 -0400

    Resolve URL for new task and add progress bar

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeNewFileFragment.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeNewFileFragment.java
@@ -9,16 +9,19 @@ package com.jefftharris.passwdsafe;
 
 import android.app.Activity;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.jefftharris.passwdsafe.file.PasswdFileData;
 import com.jefftharris.passwdsafe.file.PasswdFileUri;
+import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
 import com.jefftharris.passwdsafe.lib.view.GuiUtils;
 
 
@@ -45,8 +48,10 @@ public class PasswdSafeNewFileFragment extends Fragment
     private TextView itsFileName;
     private TextView itsPasswordEdit;
     private TextView itsPasswordConfirm;
+    private ProgressBar itsProgress;
     private Button itsOkBtn;
     private PasswdFileUri itsPasswdFileUri;
+    private ResolveTask itsResolveTask;
 
     /**
      * Create a new instance
@@ -81,7 +86,8 @@ public class PasswdSafeNewFileFragment extends Fragment
         itsPasswordEdit = (TextView)rootView.findViewById(R.id.password);
         itsPasswordConfirm =
                 (TextView)rootView.findViewById(R.id.password_confirm);
-
+        itsProgress = (ProgressBar)rootView.findViewById(R.id.progress);
+        itsProgress.setVisibility(View.INVISIBLE);
         Button cancelBtn = (Button)rootView.findViewById(R.id.cancel);
         cancelBtn.setOnClickListener(this);
         itsOkBtn = (Button)rootView.findViewById(R.id.ok);
@@ -102,10 +108,18 @@ public class PasswdSafeNewFileFragment extends Fragment
     }
 
     @Override
+    public void onStart()
+    {
+        super.onStart();
+        itsResolveTask = new ResolveTask();
+        itsResolveTask.execute();
+    }
+
+    @Override
     public void onPause()
     {
         super.onPause();
-        GuiUtils.setKeyboardVisible(itsFileName, getActivity(), false);
+        cancelOpen(false);
     }
 
     @Override
@@ -125,6 +139,101 @@ public class PasswdSafeNewFileFragment extends Fragment
             }
             break;
         }
+        }
+    }
+
+    /**
+     * Handle when the resolve task is finished
+     */
+    private void resolveTaskFinished(PasswdFileUri uri)
+    {
+        if (itsResolveTask == null) {
+            return;
+        }
+        itsResolveTask = null;
+        if (uri == null) {
+            cancelOpen(true);
+            return;
+        }
+
+        if (!uri.exists()) {
+            PasswdSafeUtil.showFatalMsg("File does't exist: " + uri,
+                                        getActivity());
+            return;
+        }
+
+        itsPasswdFileUri = uri;
+    }
+
+    /**
+     * Set whether a background task is running
+     */
+    private void setBgTaskRunning(boolean running)
+    {
+        itsFileName.setEnabled(!running);
+        itsPasswordEdit.setEnabled(!running);
+        itsPasswordConfirm.setEnabled(!running);
+        itsProgress.setVisibility(running ? View.VISIBLE : View.INVISIBLE);
+        itsOkBtn.setEnabled(!running);
+    }
+
+    /**
+     *  Cancel the file open
+     */
+    private void cancelOpen(boolean userCancel)
+    {
+        if (itsResolveTask != null) {
+            ResolveTask task = itsResolveTask;
+            itsResolveTask = null;
+            task.cancel(false);
+        }
+        GuiUtils.setKeyboardVisible(itsFileName, getActivity(), false);
+        if (userCancel && itsListener != null) {
+            itsListener.handleFileNewCanceled();
+        }
+    }
+
+    /**
+     * Background task for resolving the file URI
+     */
+    private class ResolveTask extends BackgroundTask<PasswdFileUri>
+    {
+        @Override
+        protected PasswdFileUri doInBackground(Void... voids)
+        {
+            return new PasswdFileUri(itsNewFileUri, getActivity());
+        }
+
+        @Override
+        protected void onPostExecute(PasswdFileUri uri)
+        {
+            super.onPostExecute(uri);
+            resolveTaskFinished(uri);
+        }
+    }
+
+    /**
+     * Background task
+     */
+    private abstract class BackgroundTask<ResultT>
+            extends AsyncTask<Void, Void, ResultT>
+    {
+        @Override
+        protected void onCancelled()
+        {
+            onPostExecute(null);
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            setBgTaskRunning(true);
+        }
+
+        @Override
+        protected void onPostExecute(ResultT data)
+        {
+            setBgTaskRunning(false);
         }
     }
 }
