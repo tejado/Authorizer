a6d3f1e9d6376baeea53664e3086385e4915bde2
Jeff Harris <jefftharris@gmail.com>
2015-09-06 12:30:23 -0400

    Add record viewing for an open file

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeListFragment.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeListFragment.java
@@ -8,63 +8,75 @@
 package com.jefftharris.passwdsafe;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.jefftharris.passwdsafe.view.PasswdLocation;
+import com.jefftharris.passwdsafe.view.PasswdRecordListData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *  Fragment showing lists of items from a PasswdSafe file
  */
 public class PasswdSafeListFragment extends ListFragment
-        //implements LoaderCallbacks<List<Map<String, Object>>>,
-        //implements OnClickListener
+        implements LoaderManager.LoaderCallbacks<List<PasswdRecordListData>>,
+                   View.OnClickListener
 {
     /** Listener interface for owning activity */
     public interface Listener
     {
-//        /** Mode for which items are shown from the file */
-//        public enum Mode
-//        {
-//            GROUPS,
-//            RECORDS,
-//            ALL
-//        }
-//
-//        /** Get the current record items in a background thread */
-//        List<Map<String, Object>> getBackgroundRecordItems(Mode mode);
-//
-//        /** Get the file data */
-//        PasswdFileData getFileData();
-//
-//        /** Change the location in the password file */
-//        void changeLocation(PasswdLocation location);
-//
-//        /** Update the view for the location in the password file */
-//        void updateLocationView(PasswdLocation location);
+        /** Mode for which items are shown from the file */
+        enum Mode
+        {
+            GROUPS,
+            RECORDS,
+            ALL
+        }
+
+        /** Get the current record items in a background thread */
+        List<PasswdRecordListData> getBackgroundRecordItems(Mode mode);
+
+        /** Change the location in the password file */
+        void changeLocation(PasswdLocation location);
+
+        /** Update the view for the location in the password file */
+        void updateLocationView(PasswdLocation location);
     }
 
 
-//    private Listener.Mode itsMode = Listener.Mode.GROUPS;
-//    private PasswdLocation itsLocation;
-//    private boolean itsIsContents = false;
+    private Listener.Mode itsMode = Listener.Mode.GROUPS;
+    private PasswdLocation itsLocation;
+    private boolean itsIsContents = false;
     private Listener itsListener;
-//    private ItemListAdapter itsAdapter;
-    private View itsRoot;
+    private View itsGroupPanel;
+    private TextView itsGroupLabel;
+    private ItemListAdapter itsAdapter;
 
 
     /** Create a new instance */
-    public static PasswdSafeListFragment newInstance(/*Listener.Mode mode,
+    public static PasswdSafeListFragment newInstance(Listener.Mode mode,
                                                      PasswdLocation location,
-                                                     boolean isContents*/)
+                                                     boolean isContents)
     {
         PasswdSafeListFragment frag = new PasswdSafeListFragment();
         Bundle args = new Bundle();
-//        args.putString("mode", mode.toString());
-//        args.putStringArrayList("groups", location.getGroups());
-//        args.putBoolean("isContents", isContents);
+        args.putString("mode", mode.toString());
+        args.putStringArrayList("groups", location.getGroups());
+        args.putBoolean("isContents", isContents);
         frag.setArguments(args);
         return frag;
     }
@@ -79,18 +91,18 @@ public class PasswdSafeListFragment extends ListFragment
         super.onCreate(savedInstanceState);
         Bundle args = getArguments();
 
-//        String modestr = null;
-//        ArrayList<String> groups = null;
-//        boolean isContents = false;
-//        if (args != null) {
-//            modestr = args.getString("mode");
-//            groups = args.getStringArrayList("groups");
-//            isContents = args.getBoolean("isContents", false);
-//        }
-//        itsMode = (modestr == null) ?
-//                Listener.Mode.GROUPS : Listener.Mode.valueOf(modestr);
-//        itsLocation = new PasswdLocation(groups);
-//        itsIsContents = isContents;
+        String modestr = null;
+        ArrayList<String> groups = null;
+        boolean isContents = false;
+        if (args != null) {
+            modestr = args.getString("mode");
+            groups = args.getStringArrayList("groups");
+            isContents = args.getBoolean("isContents", false);
+        }
+        itsMode = (modestr == null) ?
+                Listener.Mode.GROUPS : Listener.Mode.valueOf(modestr);
+        itsLocation = new PasswdLocation(groups);
+        itsIsContents = isContents;
     }
 
 
@@ -113,13 +125,14 @@ public class PasswdSafeListFragment extends ListFragment
                              ViewGroup container,
                              Bundle savedInstanceState)
     {
-        itsRoot = inflater.inflate(R.layout.fragment_passwdsafe_list,
-                                   container, false);
+        View root = inflater.inflate(R.layout.fragment_passwdsafe_list,
+                                     container, false);
 
-//        View groupPanel = itsRoot.findViewById(R.id.current_group_panel);
-//        groupPanel.setOnClickListener(this);
+        itsGroupPanel = root.findViewById(R.id.current_group_panel);
+        itsGroupPanel.setOnClickListener(this);
+        itsGroupLabel = (TextView)root.findViewById(R.id.current_group_label);
 
-        return itsRoot;
+        return root;
     }
 
 
@@ -130,12 +143,8 @@ public class PasswdSafeListFragment extends ListFragment
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
-//
-//        boolean hasMenu = false;
-//        setHasOptionsMenu(hasMenu);
-//
-//        itsAdapter = new ItemListAdapter(getActivity());
-//        setListAdapter(itsAdapter);
+        itsAdapter = new ItemListAdapter(getActivity());
+        setListAdapter(itsAdapter);
     }
 
 
@@ -147,12 +156,18 @@ public class PasswdSafeListFragment extends ListFragment
     {
         super.onResume();
 
-//        if (itsIsContents) {
-//            itsListener.updateLocationView(itsLocation);
-//        }
+        if (itsIsContents) {
+            itsListener.updateLocationView(itsLocation);
+        }
         refreshList();
     }
 
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        itsListener = null;
+    }
 
     /* (non-Javadoc)
      * @see android.support.v4.app.ListFragment#onListItemClick(android.widget.ListView, android.view.View, int, long)
@@ -160,44 +175,37 @@ public class PasswdSafeListFragment extends ListFragment
     @Override
     public void onListItemClick(ListView l, View v, int position, long id)
     {
-//        Map<String, Object> item = itsAdapter.getItem(position);
-//        PwsRecord rec = (PwsRecord)item.get(ParsedPasswdFileData.RECORD);
-//        if (rec != null) {
-//            String uuid = null;
-//            PasswdFileData fileData = itsListener.getFileData();
-//            if (fileData != null) {
-//                uuid = fileData.getUUID(rec);
-//            }
-//            itsListener.changeLocation(itsLocation.selectRecord(uuid));
-//        } else {
-//            String childTitle = (String)item.get(ParsedPasswdFileData.TITLE);
-//            itsListener.changeLocation(itsLocation.selectGroup(childTitle));
-//        }
+        PasswdRecordListData item = itsAdapter.getItem(position);
+        if (item.itsRecord != null) {
+            itsListener.changeLocation(itsLocation.selectRecord(item.itsUuid));
+        } else {
+            itsListener.changeLocation(itsLocation.selectGroup(item.itsTitle));
+        }
     }
 
 
     /* (non-Javadoc)
      * @see android.view.View.OnClickListener#onClick(android.view.View)
      */
-//    @Override
-//    public void onClick(View v)
-//    {
-//        switch(v.getId()) {
-//        case R.id.current_group_panel: {
-//            itsListener.changeLocation(itsLocation.popGroup());
-//            break;
-//        }
-//        }
-//    }
+    @Override
+    public void onClick(View v)
+    {
+        switch(v.getId()) {
+        case R.id.current_group_panel: {
+            itsListener.changeLocation(itsLocation.popGroup());
+            break;
+        }
+        }
+    }
 
 
     /** Update the location shown by the list */
-//    public void updateLocationView(PasswdLocation location, Listener.Mode mode)
-//    {
-//        itsLocation = location;
-//        itsMode = mode;
-//        refreshList();
-//    }
+    public void updateLocationView(PasswdLocation location, Listener.Mode mode)
+    {
+        itsLocation = location;
+        itsMode = mode;
+        refreshList();
+    }
 
 
     /** Refresh the list due to file changes */
@@ -207,181 +215,178 @@ public class PasswdSafeListFragment extends ListFragment
             return;
         }
 
-//        LoaderManager lm = getLoaderManager();
-//        lm.restartLoader(0, null, this);
-//
-//        boolean groupVisible = false;
-//        switch (itsMode) {
-//        case GROUPS: {
-//            groupVisible = true;
-//            break;
-//        }
-//        case RECORDS: {
-//            groupVisible = false;
-//            break;
-//        }
-//        case ALL: {
-//            groupVisible = true;
-//            break;
-//        }
-//        }
-//
-//        if (groupVisible) {
-//            String groupPath = itsLocation.getGroupPath();
-//            if (TextUtils.isEmpty(groupPath)) {
-//                groupVisible = false;
-//            } else {
-//                TextView tv = (TextView)
-//                        itsRoot.findViewById(R.id.current_group_label);
-//                tv.setText(groupPath);
-//            }
-//        }
-//
-//        View groupPanel = itsRoot.findViewById(R.id.current_group_panel);
-//        groupPanel.setVisibility(groupVisible ? View.VISIBLE : View.GONE);
+        LoaderManager lm = getLoaderManager();
+        lm.restartLoader(0, null, this);
+
+        boolean groupVisible = false;
+        switch (itsMode) {
+        case GROUPS: {
+            groupVisible = true;
+            break;
+        }
+        case RECORDS: {
+            groupVisible = false;
+            break;
+        }
+        case ALL: {
+            groupVisible = true;
+            break;
+        }
+        }
+
+        if (groupVisible) {
+            String groupPath = itsLocation.getGroupPath();
+            if (TextUtils.isEmpty(groupPath)) {
+                groupVisible = false;
+            } else {
+                itsGroupLabel.setText(groupPath);
+            }
+        }
+
+        itsGroupPanel.setVisibility(groupVisible ? View.VISIBLE : View.GONE);
     }
 
 
     /* (non-Javadoc)
      * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onCreateLoader(int, android.os.Bundle)
      */
-//    @Override
-//    public Loader<List<Map<String, Object>>> onCreateLoader(int id, Bundle args)
-//    {
-//        return new ItemLoader(itsMode, itsListener, getActivity());
-//    }
+    @Override
+    public Loader<List<PasswdRecordListData>> onCreateLoader(int id, Bundle args)
+    {
+        return new ItemLoader(itsMode, itsListener, getActivity());
+    }
 
 
     /* (non-Javadoc)
      * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onLoadFinished(android.support.v4.content.Loader, java.lang.Object)
      */
-//    @Override
-//    public void onLoadFinished(Loader<List<Map<String, Object>>> loader,
-//                               List<Map<String, Object>> data)
-//    {
-//        itsAdapter.setData(data);
-//    }
+    @Override
+    public void onLoadFinished(Loader<List<PasswdRecordListData>> loader,
+                               List<PasswdRecordListData> data)
+    {
+        itsAdapter.setData(data);
+    }
 
 
     /* (non-Javadoc)
      * @see android.support.v4.app.LoaderManager.LoaderCallbacks#onLoaderReset(android.support.v4.content.Loader)
      */
-//    @Override
-//    public void onLoaderReset(Loader<List<Map<String, Object>>> loader)
-//    {
-//        itsAdapter.setData(null);
-//    }
+    @Override
+    public void onLoaderReset(Loader<List<PasswdRecordListData>> loader)
+    {
+        itsAdapter.setData(null);
+    }
 
 
     /** List adapter for file items */
-//    private static class ItemListAdapter
-//            extends ArrayAdapter<Map<String, Object>>
-//    {
-//        private final LayoutInflater itsInflater;
-//
-//        /** Constructor */
-//        public ItemListAdapter(Context context)
-//        {
-//            super(context, R.layout.passwdsafe_list_item, android.R.id.text1);
-//            itsInflater = (LayoutInflater)context.getSystemService(
-//                    Context.LAYOUT_INFLATER_SERVICE);
-//        }
-//
-//        /** Set the list data */
-//        public void setData(List<Map<String, Object>> data)
-//        {
-//            clear();
-//            if (data != null) {
-//                for (Map<String, Object> item: data) {
-//                    add(item);
-//                }
-//            }
-//        }
-//
-//        /* (non-Javadoc)
-//         * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
-//         */
-//        @Override
-//        public View getView(int position, View convertView, ViewGroup parent)
-//        {
-//            View view;
-//            if (convertView == null) {
-//                view = itsInflater.inflate(R.layout.passwdsafe_list_item,
-//                                           parent, false);
-//            } else {
-//                view = convertView;
-//            }
-//
-//            Map<String, Object> item = getItem(position);
-//            String title = (String)item.get(ParsedPasswdFileData.TITLE);
-//            String user = (String)item.get(ParsedPasswdFileData.USERNAME);
-//            Integer icon = (Integer)item.get(ParsedPasswdFileData.ICON);
-//            String match = (String)item.get(ParsedPasswdFileData.MATCH);
-//            setTextView(view, android.R.id.text1, title);
-//            setTextView(view, android.R.id.text2, user);
-//            setTextView(view, R.id.match, match);
-//            ((ImageView)view.findViewById(R.id.icon)).setImageResource(icon);
-//            return view;
-//        }
-//
-//        /** Set a text view */
-//        private static void setTextView(View view, int textId, String str)
-//        {
-//            if (str == null) {
-//                str = "";
-//            }
-//            ((TextView)view.findViewById(textId)).setText(str);
-//        }
-//    }
+    private static class ItemListAdapter
+            extends ArrayAdapter<PasswdRecordListData>
+    {
+        private final LayoutInflater itsInflater;
+
+        /** Constructor */
+        public ItemListAdapter(Context context)
+        {
+            super(context, R.layout.passwdsafe_list_item, android.R.id.text1);
+            itsInflater = (LayoutInflater)context.getSystemService(
+                    Context.LAYOUT_INFLATER_SERVICE);
+
+            // TODO: section list
+        }
+
+        /** Set the list data */
+        public void setData(List<PasswdRecordListData> data)
+        {
+            clear();
+            if (data != null) {
+                for (PasswdRecordListData item: data) {
+                    add(item);
+                }
+            }
+        }
+
+        /* (non-Javadoc)
+         * @see android.widget.ArrayAdapter#getView(int, android.view.View, android.view.ViewGroup)
+         */
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            View view;
+            if (convertView == null) {
+                view = itsInflater.inflate(R.layout.passwdsafe_list_item,
+                                           parent, false);
+            } else {
+                view = convertView;
+            }
+
+            // TODO: view holder
+
+            PasswdRecordListData item = getItem(position);
+            setTextView(view, android.R.id.text1, item.itsTitle);
+            setTextView(view, android.R.id.text2, item.itsUser);
+            setTextView(view, R.id.match, item.itsMatch);
+            ((ImageView)view.findViewById(R.id.icon)).setImageResource(item.itsIcon);
+            return view;
+        }
+
+        /** Set a text view */
+        private static void setTextView(View view, int textId, String str)
+        {
+            if (str == null) {
+                str = "";
+            }
+            ((TextView)view.findViewById(textId)).setText(str);
+        }
+    }
 
 
     /** Loader for file items */
-//    private static class ItemLoader
-//            extends AsyncTaskLoader<List<Map<String, Object>>>
-//    {
-//        private final Listener.Mode itsMode;
-//        private final Listener itsActListener;
-//
-//        /** Constructor */
-//        public ItemLoader(Listener.Mode mode, Listener actListener,
-//                          Context context)
-//        {
-//            super(context);
-//            itsMode = mode;
-//            itsActListener = actListener;
-//        }
-//
-//
-//        /** Handle when the loader is reset */
-//        @Override
-//        protected void onReset()
-//        {
-//            super.onReset();
-//            onStopLoading();
-//        }
-//
-//        /** Handle when the loader is started */
-//        @Override
-//        protected void onStartLoading()
-//        {
-//            forceLoad();
-//        }
-//
-//        /** Handle when the loader is stopped */
-//        @Override
-//        protected void onStopLoading()
-//        {
-//            cancelLoad();
-//        }
-//
-//        /* (non-Javadoc)
-//         * @see android.support.v4.content.AsyncTaskLoader#loadInBackground()
-//         */
-//        @Override
-//        public List<Map<String, Object>> loadInBackground()
-//        {
-//            return itsActListener.getBackgroundRecordItems(itsMode);
-//        }
-//    }
+    private static class ItemLoader
+            extends AsyncTaskLoader<List<PasswdRecordListData>>
+    {
+        private final Listener.Mode itsMode;
+        private final Listener itsActListener;
+
+        /** Constructor */
+        public ItemLoader(Listener.Mode mode, Listener actListener,
+                          Context context)
+        {
+            super(context);
+            itsMode = mode;
+            itsActListener = actListener;
+        }
+
+
+        /** Handle when the loader is reset */
+        @Override
+        protected void onReset()
+        {
+            super.onReset();
+            onStopLoading();
+        }
+
+        /** Handle when the loader is started */
+        @Override
+        protected void onStartLoading()
+        {
+            forceLoad();
+        }
+
+        /** Handle when the loader is stopped */
+        @Override
+        protected void onStopLoading()
+        {
+            cancelLoad();
+        }
+
+        /* (non-Javadoc)
+         * @see android.support.v4.content.AsyncTaskLoader#loadInBackground()
+         */
+        @Override
+        public List<PasswdRecordListData> loadInBackground()
+        {
+            return itsActListener.getBackgroundRecordItems(itsMode);
+        }
+    }
 }
 
