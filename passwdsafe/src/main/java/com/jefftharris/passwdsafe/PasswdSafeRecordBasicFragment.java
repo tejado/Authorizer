bea0d3d93137973eb2517238f4b5bd6c751162e7
Jeff Harris <jefftharris@gmail.com>
2015-09-07 21:39:01 -0400

    Add user field to basic record view

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeRecordBasicFragment.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeRecordBasicFragment.java
@@ -12,11 +12,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jefftharris.passwdsafe.file.PasswdFileData;
+import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
+
+import org.pwsafe.lib.file.PwsRecord;
 
 
 /**
@@ -34,6 +40,8 @@ public class PasswdSafeRecordBasicFragment extends Fragment
     }
 
     private String itsRecUuid;
+    private View itsUserRow;
+    private TextView itsUser;
     private Listener itsListener;
 
     public static PasswdSafeRecordBasicFragment newInstance(String recUuid)
@@ -66,10 +74,12 @@ public class PasswdSafeRecordBasicFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
+        setHasOptionsMenu(true);
         View root = inflater.inflate(R.layout.fragment_passwdsafe_record_basic,
                                      container, false);
+        itsUserRow = root.findViewById(R.id.user_row);
+        itsUser = (TextView)root.findViewById(R.id.user);
 
-        ((TextView)root).setText("Rec " + itsRecUuid);
         return root;
     }
 
@@ -87,10 +97,60 @@ public class PasswdSafeRecordBasicFragment extends Fragment
         itsListener = null;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.fragment_passwdsafe_record_basic, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()) {
+        case R.id.menu_copy_user: {
+            PasswdSafeUtil.copyToClipboard(itsUser.getText().toString(),
+                                           getActivity());
+            return true;
+        }
+        default: {
+            return super.onOptionsItemSelected(item);
+        }
+        }
+    }
+
     /**
      * Refresh the view
      */
     private void refresh()
     {
+        if (!isAdded() || (itsListener == null)) {
+            return;
+        }
+
+        PasswdFileData fileData = itsListener.getFileData();
+        if (fileData == null) {
+            return;
+        }
+
+        PwsRecord rec = fileData.getRecord(itsRecUuid);
+        if (rec == null) {
+            return;
+        }
+
+        setFieldText(itsUser, itsUserRow, fileData.getUsername(rec));
+    }
+
+    /**
+     * Set the value of a text field.  The field's row is visible if the text
+     * isn't null.
+     */
+    private void setFieldText(TextView field, View fieldRow, String text)
+    {
+        field.setText(text);
+
+        if (fieldRow != null) {
+            fieldRow.setVisibility((text != null) ? View.VISIBLE : View.GONE);
+        }
     }
 }
