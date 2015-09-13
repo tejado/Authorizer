087240a52fa1240c9eb1ae7c6e01385c6b4db738
Jeff Harris <jefftharris@users.noreply.github.com>
2015-09-13 14:28:50 -0400

    Add notes to record view

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeRecordNotesFragment.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeRecordNotesFragment.java
@@ -8,6 +8,9 @@
 package com.jefftharris.passwdsafe;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,6 +18,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
+import com.jefftharris.passwdsafe.lib.view.GuiUtils;
 
 
 /**
@@ -23,6 +30,13 @@ import android.view.ViewGroup;
 public class PasswdSafeRecordNotesFragment
         extends AbstractPasswdSafeRecordFragment
 {
+    private boolean itsIsWordWrap = true;
+    private boolean itsIsMonospace = true;
+    private TextView itsNotes;
+
+    private static final String WORD_WRAP_PREF = "wordwrap";
+    private static final String MONOSPACE_PREF = "monospace";
+
     /**
      * Create a new instance of the fragment
      */
@@ -40,17 +54,34 @@ public class PasswdSafeRecordNotesFragment
                              Bundle savedInstanceState)
     {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.fragment_passwdsafe_record_notes,
-                                container, false);
+        View root = inflater.inflate(R.layout.fragment_passwdsafe_record_notes,
+                                     container, false);
+        itsNotes = (TextView)root.findViewById(R.id.notes);
+        GuiUtils.setTextSelectable(itsNotes);
+        return root;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // TODO: menu copy notes
-        // TODO: menu toggle monospace
-        // TODO: menu toggle wrap notes
         switch (item.getItemId()) {
+        case R.id.menu_copy_notes: {
+            PasswdSafeUtil.copyToClipboard(itsNotes.getText().toString(),
+                                           getActivity());
+            return true;
+        }
+        case R.id.menu_toggle_monospace: {
+            itsIsMonospace = !itsIsMonospace;
+            saveNotesOptionsPrefs();
+            setNotesOptions();
+            return true;
+        }
+        case R.id.menu_toggle_word_wrap: {
+            itsIsWordWrap = !itsIsWordWrap;
+            saveNotesOptionsPrefs();
+            setNotesOptions();
+            return true;
+        }
         default: {
             return super.onOptionsItemSelected(item);
         }
@@ -66,5 +97,50 @@ public class PasswdSafeRecordNotesFragment
     @Override
     protected void doRefresh()
     {
+        RecordInfo info = getRecordInfo();
+        if (info == null) {
+            return;
+        }
+
+        switch (info.itsPasswdRec.getType()) {
+        case NORMAL:
+        case ALIAS: {
+            String notes = info.itsFileData.getNotes(info.itsRec);
+            itsNotes.setText(notes);
+            break;
+        }
+        case SHORTCUT: {
+            break;
+        }
+        }
+
+        SharedPreferences prefs =
+                getActivity().getPreferences(Context.MODE_PRIVATE);
+        itsIsWordWrap = prefs.getBoolean(WORD_WRAP_PREF, true);
+        itsIsMonospace = prefs.getBoolean(MONOSPACE_PREF, false);
+        setNotesOptions();
+    }
+
+    /**
+     * Save the notes preferences
+     */
+    private void saveNotesOptionsPrefs()
+    {
+        SharedPreferences prefs =
+                getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(WORD_WRAP_PREF, itsIsWordWrap);
+        editor.putBoolean(MONOSPACE_PREF, itsIsMonospace);
+        editor.apply();
+    }
+
+    /**
+     * Set options for the notes
+     */
+    private void setNotesOptions()
+    {
+        itsNotes.setHorizontallyScrolling(!itsIsWordWrap);
+        itsNotes.setTypeface(
+                itsIsMonospace ? Typeface.MONOSPACE : Typeface.DEFAULT);
     }
 }
