4270c645fb809cdbf84e1ecf575419d2cf6cad75
Jeff Harris <jefftharris@gmail.com>
2015-09-27 10:28:26 -0400

    Show title in record edit fragment

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeEditRecordFragment.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafeEditRecordFragment.java
@@ -8,8 +8,9 @@
 package com.jefftharris.passwdsafe;
 
 
-import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,8 +18,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.jefftharris.passwdsafe.file.PasswdFileData;
+import com.jefftharris.passwdsafe.file.PasswdRecord;
 import com.jefftharris.passwdsafe.view.PasswdLocation;
+
+import org.pwsafe.lib.file.PwsRecord;
 
 
 /**
@@ -31,6 +37,9 @@ public class PasswdSafeEditRecordFragment extends Fragment
      */
     public interface Listener
     {
+        /** Get the file data */
+        PasswdFileData getFileData();
+
         /** Update the view for editing a record */
         void updateViewEditRecord(PasswdLocation location);
 
@@ -40,8 +49,11 @@ public class PasswdSafeEditRecordFragment extends Fragment
 
     private PasswdLocation itsLocation;
     private Listener itsListener;
+    private TextView itsTitle;
 
     // TODO: if pending changes, warn on navigation
+    // TODO: v2 support
+    // TODO: protected flag
 
     /**
      * Create a new instance
@@ -71,10 +83,10 @@ public class PasswdSafeEditRecordFragment extends Fragment
     }
 
     @Override
-    public void onAttach(Activity activity)
+    public void onAttach(Context ctx)
     {
-        super.onAttach(activity);
-        itsListener = (Listener)activity;
+        super.onAttach(ctx);
+        itsListener = (Listener)ctx;
     }
 
     @Override
@@ -84,6 +96,9 @@ public class PasswdSafeEditRecordFragment extends Fragment
         setHasOptionsMenu(true);
         View rootView = inflater.inflate(
                 R.layout.fragment_passwdsafe_edit_record, container, false);
+        itsTitle = (TextView)rootView.findViewById(R.id.title);
+
+        initialize();
 
         return rootView;
     }
@@ -127,13 +142,72 @@ public class PasswdSafeEditRecordFragment extends Fragment
     }
 
     /**
+     * Initialize the view
+     */
+    private void initialize()
+    {
+        RecordInfo info = getRecordInfo();
+        if (info == null) {
+            return;
+        }
+
+        itsTitle.setText(info.itsFileData.getTitle(info.itsRec));
+    }
+
+    /**
      * Refresh the view
      */
     private void refresh()
     {
+        // TODO: refresh needed?
+        /*
         if (!isAdded() || (itsListener == null)) {
             return;
         }
+        */
+    }
 
+    /**
+     * Get the record information
+     */
+    protected RecordInfo getRecordInfo()
+    {
+        // TODO: pull record info into base class to share with AbstractPasswdSafeRecordFragment
+
+        if (isAdded() && (itsListener != null)) {
+            PasswdFileData fileData = itsListener.getFileData();
+            if (fileData != null) {
+                PwsRecord rec = fileData.getRecord(itsLocation.getRecord());
+                if (rec != null) {
+                    PasswdRecord passwdRec = fileData.getPasswdRecord(rec);
+                    if (passwdRec != null) {
+                        return new RecordInfo(rec, passwdRec, fileData);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Wrapper class for record information
+     */
+    protected static class RecordInfo
+    {
+        public final PwsRecord itsRec;
+        public final PasswdRecord itsPasswdRec;
+        public final PasswdFileData itsFileData;
+
+        /**
+         * Constructor
+         */
+        public RecordInfo(@NonNull PwsRecord rec,
+                          @NonNull PasswdRecord passwdRec,
+                          @NonNull PasswdFileData fileData)
+        {
+            itsRec = rec;
+            itsPasswdRec = passwdRec;
+            itsFileData = fileData;
+        }
     }
 }
