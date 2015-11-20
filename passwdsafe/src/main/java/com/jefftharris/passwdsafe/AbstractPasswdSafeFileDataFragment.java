0b7d3419f7c32548b645f81c5aaf71e8d521dc18
Jeff Harris <jefftharris@gmail.com>
2015-11-20 11:16:22 -0500

    Split AbstractPasswdSafeRecordFragment into base class for fragments referencing file data

---
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/AbstractPasswdSafeFileDataFragment.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (©) 2015 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
+package com.jefftharris.passwdsafe;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.jefftharris.passwdsafe.file.PasswdFileData;
+import com.jefftharris.passwdsafe.file.PasswdRecord;
+
+import org.pwsafe.lib.file.PwsRecord;
+
+/**
+ * Base fragment for accessing password file data
+ */
+public abstract class AbstractPasswdSafeFileDataFragment
+        <ListenerT extends AbstractPasswdSafeFileDataFragment.Listener>
+        extends Fragment
+{
+    /**
+     * Listener interface for owning activity
+     */
+    public interface Listener
+    {
+        /** Get the file data */
+        PasswdFileData getFileData();
+
+        /** Is the navigation drawer open */
+        boolean isNavDrawerOpen();
+    }
+
+    /**
+     * Wrapper class for record information
+     */
+    protected static class RecordInfo
+    {
+        public final PwsRecord itsRec;
+        public final PasswdRecord itsPasswdRec;
+        public final PasswdFileData itsFileData;
+
+        /**
+         * Constructor
+         */
+        public RecordInfo(@NonNull PwsRecord rec,
+                          @NonNull PasswdRecord passwdRec,
+                          @NonNull PasswdFileData fileData)
+        {
+            itsRec = rec;
+            itsPasswdRec = passwdRec;
+            itsFileData = fileData;
+        }
+    }
+
+    protected String itsRecUuid;
+    protected ListenerT itsListener;
+
+    @Override
+    public void onAttach(Context ctx)
+    {
+        super.onAttach(ctx);
+        //noinspection unchecked
+        itsListener = (ListenerT)ctx;
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+        itsListener = null;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
+    {
+        if ((itsListener != null) && !itsListener.isNavDrawerOpen()) {
+            doOnCreateOptionsMenu(menu, inflater);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    /**
+     * Derived-class create options menu
+     */
+    protected abstract void doOnCreateOptionsMenu(Menu menu,
+                                                  MenuInflater inflater);
+
+    /**
+     * Get the record information
+     */
+    protected RecordInfo getRecordInfo()
+    {
+        if (isAdded() && (itsListener != null)) {
+            PasswdFileData fileData = itsListener.getFileData();
+            if (fileData != null) {
+                PwsRecord rec = fileData.getRecord(itsRecUuid);
+                if (rec != null) {
+                    PasswdRecord passwdRec = fileData.getPasswdRecord(rec);
+                    if (passwdRec != null) {
+                        return new RecordInfo(rec, passwdRec, fileData);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
