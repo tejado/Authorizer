ca649ef160642bc8b7c3480412638360176e6f70
Jeff Harris <jefftharris@users.noreply.github.com>
2015-12-26 20:47:00 -0500

    Use trash can for delete instead of X

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafe.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/PasswdSafe.java
@@ -73,7 +73,6 @@ public class PasswdSafe extends AppCompatActivity
     // TODO: expired passwords
     // TODO: expiry notifications
     // TODO: recheck all icons (remove use of all built-in ones)
-    // TODO: use trash can icon for delete and X for close for consistency
     // TODO: autobackup
     // TODO: shortcuts
     // TODO: check manifest errors regarding icons
