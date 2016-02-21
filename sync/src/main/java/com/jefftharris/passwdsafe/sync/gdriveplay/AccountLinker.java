7a7cbd91d71546155cbbcd0862620b7355f373fd
Jeff Harris <jefftharris@users.noreply.github.com>
2016-02-21 14:26:35 -0500

    Handle account add and remove for Google Drive Play

--- sync/src/main/java/com/jefftharris/passwdsafe/sync/gdriveplay/AccountLinker.java
+++ sync/src/main/java/com/jefftharris/passwdsafe/sync/gdriveplay/AccountLinker.java
@@ -8,13 +8,17 @@ package com.jefftharris.passwdsafe.sync.gdriveplay;
 
 import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Pair;
@@ -22,12 +26,10 @@ import android.util.Pair;
 import com.google.android.gms.auth.GoogleAuthUtil;
 import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
-import com.jefftharris.passwdsafe.lib.ProviderType;
 import com.jefftharris.passwdsafe.sync.R;
-import com.jefftharris.passwdsafe.sync.lib.NewAccountTask;
 
 /**
  * The AccountLinker class manages a new connection to a GDrive account
@@ -42,11 +44,12 @@ public class AccountLinker
     {
         CHOOSING_ACCOUNT,
         CONNECTING,
+        RESOLVING_CONNECTION_ERROR,
         CONNECTED,
         FINISHED
     }
 
-    private final Activity itsActivity;
+    private final FragmentActivity itsActivity;
     private final int itsRequestCode;
     private final Context itsContext;
     private AccountLinker.State itsState = State.CHOOSING_ACCOUNT;
@@ -54,7 +57,7 @@ public class AccountLinker
     private String itsAcctName = null;
 
     /** Constructor */
-    public AccountLinker(Activity act, int requestCode, Context ctx)
+    public AccountLinker(FragmentActivity act, int requestCode, Context ctx)
     {
         itsActivity = act;
         itsRequestCode = requestCode;
@@ -62,33 +65,46 @@ public class AccountLinker
         evalState();
     }
 
-    /* (non-Javadoc)
-     * @see com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener#onConnectionFailed(com.google.android.gms.common.ConnectionResult)
-     */
     @Override
-    public void onConnectionFailed(ConnectionResult result)
+    public void onConnectionFailed(@NonNull ConnectionResult result)
     {
         PasswdSafeUtil.dbginfo(TAG, "onConnectionFailed resolution %b, %s",
                                result.hasResolution(), result);
-        if (!result.hasResolution()) {
-            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
-                                                  itsActivity, 0).show();
-            itsState = State.FINISHED;
-            sendActivityResult(Activity.RESULT_CANCELED);
-            return;
+        switch (itsState) {
+        case CONNECTING: {
+            if (result.hasResolution()) {
+                try {
+                    itsState = State.RESOLVING_CONNECTION_ERROR;
+                    result.startResolutionForResult(itsActivity,
+                                                    itsRequestCode);
+                } catch (SendIntentException e) {
+                    Log.e(TAG, "Error starting resolution", e);
+                    itsState = State.FINISHED;
+                    sendActivityResult(Activity.RESULT_CANCELED);
+                }
+            } else {
+                itsState = State.RESOLVING_CONNECTION_ERROR;
+
+                Bundle args = new Bundle();
+                args.putInt(ErrorDialogFragment.ERROR_CODE,
+                            result.getErrorCode());
+                args.putInt(ErrorDialogFragment.REQUEST_CODE, itsRequestCode);
+                DialogFragment dlgFrag = new ErrorDialogFragment();
+                dlgFrag.setArguments(args);
+                dlgFrag.show(itsActivity.getSupportFragmentManager(),
+                             "errorDialog");
+            }
+            break;
         }
-        try {
-            result.startResolutionForResult(itsActivity, itsRequestCode);
-        } catch (SendIntentException e) {
-            Log.e(TAG, "Error starting resolution", e);
-            itsState = State.FINISHED;
-            sendActivityResult(Activity.RESULT_CANCELED);
+        case CHOOSING_ACCOUNT:
+        case RESOLVING_CONNECTION_ERROR:
+        case CONNECTED:
+        case FINISHED: {
+            break;
+        }
         }
     }
 
-    /* (non-Javadoc)
-     * @see com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks#onConnected(android.os.Bundle)
-     */
     @Override
     public void onConnected(Bundle hint)
     {
@@ -98,9 +114,6 @@ public class AccountLinker
         sendActivityResult(Activity.RESULT_OK);
     }
 
-    /* (non-Javadoc)
-     * @see com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks#onConnectionSuspended(int)
-     */
     @Override
     public void onConnectionSuspended(int cause)
     {
@@ -108,16 +121,14 @@ public class AccountLinker
     }
 
     /** Handle an activity result to further the state of the linker */
-    public Pair<Boolean, NewAccountTask>
-    handleActivityResult(int activityResult,
-                         Intent activityData,
-                         Uri providerAcctUri)
+    public Pair<Boolean, String>
+    handleActivityResult(int activityResult, Intent activityData)
     {
         PasswdSafeUtil.dbginfo(
                 TAG, "handleActivityResult state %s rc %d, data %s",
                 itsState, activityResult, activityData);
         boolean finished = true;
-        NewAccountTask task = null;
+        String connectedAcctName = null;
         switch (itsState) {
         case CHOOSING_ACCOUNT: {
             if (activityData == null) {
@@ -138,8 +149,10 @@ public class AccountLinker
             evalState();
             break;
         }
-        case CONNECTING: {
+        case CONNECTING:
+        case RESOLVING_CONNECTION_ERROR: {
             if (activityResult == Activity.RESULT_OK) {
+                itsState = State.CONNECTING;
                 finished = false;
                 evalState();
             } else {
@@ -148,18 +161,14 @@ public class AccountLinker
             break;
         }
         case CONNECTED: {
-            // TODO play: use updated play services and acct id for unique value
-            // with name as display name
-            task = new NewAccountTask(providerAcctUri, itsAcctName,
-                                      ProviderType.GDRIVE_PLAY,
-                                      false, itsContext);
+            connectedAcctName = itsAcctName;
             break;
         }
         case FINISHED: {
             break;
         }
         }
-        return new Pair<>(finished, task);
+        return new Pair<>(finished, connectedAcctName);
     }
 
     /** Disconnect the linker and shut down */
@@ -195,9 +204,12 @@ public class AccountLinker
                 itsClient = GDrivePlayProvider.createClient(
                         itsContext, itsAcctName, this, this);
             }
-            itsClient.connect();
+            if (!itsClient.isConnecting() && !itsClient.isConnected()) {
+                itsClient.connect();
+            }
             break;
         }
+        case RESOLVING_CONNECTION_ERROR:
         case CONNECTED:
         case FINISHED: {
             break;
@@ -211,12 +223,50 @@ public class AccountLinker
      */
     private void sendActivityResult(int rc)
     {
-        PendingIntent intent = itsActivity.createPendingResult(
-                itsRequestCode, new Intent(), PendingIntent.FLAG_ONE_SHOT);
+        sendActivityResult(itsActivity, itsRequestCode, rc);
+    }
+
+    /**
+     * Send a result event to the given activity
+     */
+    private static void sendActivityResult(Activity act, int requestCode,
+                                           int resultCode)
+    {
+        PendingIntent intent = act.createPendingResult(
+                requestCode, new Intent(), PendingIntent.FLAG_ONE_SHOT);
         try {
-            intent.send(rc);
+            intent.send(resultCode);
         } catch (PendingIntent.CanceledException e) {
             Log.e(TAG, "intent send failed", e);
+        }
+    }
+
+    /**
+     * Dialog fragment for showing a connection error
+     */
+    public static class ErrorDialogFragment extends DialogFragment
+    {
+        public static final String ERROR_CODE = "errorCode";
+        public static final String REQUEST_CODE = "requestCode";
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            Bundle args = getArguments();
+            int errorCode = args.getInt(ERROR_CODE);
+            int requestCode = args.getInt(REQUEST_CODE);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    getActivity(), errorCode, requestCode);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog)
+        {
+            Bundle args = getArguments();
+            int requestCode = args.getInt(REQUEST_CODE);
+            sendActivityResult(getActivity(), requestCode,
+                               Activity.RESULT_CANCELED);
         }
     }
 }
