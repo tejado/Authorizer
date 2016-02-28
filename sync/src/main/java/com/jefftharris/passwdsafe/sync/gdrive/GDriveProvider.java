ef742bff23ff51544ec34c809395b6736ac13bf0
Jeff Harris <jefftharris@gmail.com>
2016-02-28 12:45:36 -0500

    Migrate account name preference from previous version

--- sync/src/main/java/com/jefftharris/passwdsafe/sync/gdrive/GDriveProvider.java
+++ sync/src/main/java/com/jefftharris/passwdsafe/sync/gdrive/GDriveProvider.java
@@ -13,6 +13,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
@@ -36,6 +37,7 @@ import com.jefftharris.passwdsafe.sync.SyncUpdateHandler;
 import com.jefftharris.passwdsafe.sync.lib.AbstractProvider;
 import com.jefftharris.passwdsafe.sync.lib.DbProvider;
 import com.jefftharris.passwdsafe.sync.lib.NewAccountTask;
+import com.jefftharris.passwdsafe.sync.lib.SyncDb;
 import com.jefftharris.passwdsafe.sync.lib.SyncLogRecord;
 
 /**
@@ -51,6 +53,9 @@ public class GDriveProvider extends AbstractProvider
             "application/vnd.google-apps.folder";
 
     private static final String PREF_ACCOUNT_NAME = "gdriveAccountName";
+    private static final String PREF_MIGRATION = "gdriveMigration";
+
+    private static final int MIGRATION_V3API = 1;
 
     private static final String TAG = "GDriveProvider";
 
@@ -68,6 +73,7 @@ public class GDriveProvider extends AbstractProvider
     @Override
     public void init()
     {
+        checkMigration();
         updateAcct();
         requestSync(false);
     }
@@ -231,5 +237,37 @@ public class GDriveProvider extends AbstractProvider
         SharedPreferences prefs =
                 PreferenceManager.getDefaultSharedPreferences(itsContext);
         itsAccountName = prefs.getString(PREF_ACCOUNT_NAME, null);
+    }
+
+    /**
+     * Check whether any migrations are needed
+     */
+    private void checkMigration()
+    {
+        SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(itsContext);
+        int migration = prefs.getInt(PREF_MIGRATION, 0);
+
+        if (migration < MIGRATION_V3API) {
+            // Set the account name from the db provider
+            SyncDb syncDb = SyncDb.acquire();
+            try {
+                SQLiteDatabase db = syncDb.beginTransaction();
+                for (DbProvider provider: SyncDb.getProviders(db)) {
+                    if (provider.itsType != ProviderType.GDRIVE) {
+                        continue;
+                    }
+
+                    setAcctName(provider.itsAcct);
+                }
+                db.setTransactionSuccessful();
+            } catch (SQLException e) {
+                Log.e(TAG, "Error migrating account", e);
+            } finally {
+                syncDb.endTransactionAndRelease();
+            }
+
+            prefs.edit().putInt(PREF_MIGRATION, MIGRATION_V3API).apply();
+        }
     }
 }
