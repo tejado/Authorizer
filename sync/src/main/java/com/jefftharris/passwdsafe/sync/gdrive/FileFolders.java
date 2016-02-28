da507802fffd0e7ec7bef05abdc48b3c1c6c2ddc
Jeff Harris <jefftharris@gmail.com>
2016-02-28 17:10:50 -0500

    Simplify FileFolders for transient, single file operation
    Remove the bulk compute method.  Since the class isn't kept permanently by the syncer, it doesn't need to prune cached entries when no longer needed.

--- sync/src/main/java/com/jefftharris/passwdsafe/sync/gdrive/FileFolders.java
+++ sync/src/main/java/com/jefftharris/passwdsafe/sync/gdrive/FileFolders.java
@@ -10,15 +10,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 import android.text.TextUtils;
 
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
-import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
 
 /**
  * The FileFolders class encapsulates handling of folders for GDrive files
@@ -26,27 +22,13 @@ import com.jefftharris.passwdsafe.lib.PasswdSafeUtil;
 public class FileFolders
 {
     private final Drive itsDrive;
-    private final HashMap<String, File> itsFileCache;
-    private final HashMap<String, FolderRefs> itsFolderRefs;
-
-    private static final String TAG = "FileFolders";
-
-    /** Constructor with external cache and folder refs collections */
-    public FileFolders(Drive drive,
-                       HashMap<String, File> fileCache,
-                       HashMap<String, FolderRefs> folderRefs)
-    {
-        itsDrive = drive;
-        itsFileCache = fileCache;
-        itsFolderRefs = folderRefs;
-    }
-
+    private final HashMap<String, File> itsFileCache = new HashMap<>();
+    private final HashMap<String, FolderRefs> itsFolderRefs = new HashMap<>();
 
     /** Constructor with internal helper collections */
     public FileFolders(Drive drive)
     {
-        this(drive, new HashMap<String, File>(),
-                new HashMap<String, FolderRefs>());
+        itsDrive = drive;
     }
 
 
@@ -54,67 +36,13 @@ public class FileFolders
     public String computeFileFolders(File file)
             throws IOException
     {
-        String id = file.getId();
-        Map<String, String> folders = computeFilesFolders(
-                Collections.singletonMap(id, file));
-        return folders.get(id);
-    }
-
-
-    /** Compute the folders for the given files */
-    public Map<String, String>
-    computeFilesFolders(Map<String, File> remfiles)
-            throws IOException
-    {
-        HashMap<String, String> fileFolders = new HashMap<>();
-        for (File remfile: remfiles.values()) {
-            if (remfile == null) {
-                continue;
-            }
-
-            // Remove the file from the folder refs to handle moves.
-            // The file will be re-added to the correct refs
-            String id = remfile.getId();
-            for (FolderRefs refs: itsFolderRefs.values()) {
-                refs.removeRef(id);
-            }
-
-            String folders = computeFolders(remfile);
-            fileFolders.put(id, folders);
-        }
-        // Purge empty folder references
-        Iterator<Map.Entry<String, FolderRefs>> iter =
-                itsFolderRefs.entrySet().iterator();
-        while (iter.hasNext()) {
-            Map.Entry<String, FolderRefs> entry = iter.next();
-            Set<String> fileRefs = entry.getValue().itsFileRefs;
-            if (PasswdSafeUtil.DEBUG) {
-                PasswdSafeUtil.dbginfo(TAG, "cached folder %s, refs [%s]",
-                        entry.getKey(), TextUtils.join(", ", fileRefs));
-            }
-            if (fileRefs.isEmpty()) {
-                iter.remove();
-            }
-        }
-
-        return fileFolders;
-    }
-
-
-    /** Internal computation for the folders of a file */
-    private String computeFolders(File remfile)
-            throws IOException
-    {
-        String fileId = remfile.getId();
+        String fileId = file.getId();
         ArrayList<String> folders = new ArrayList<>();
-        for (String parentId: remfile.getParents()) {
+        for (String parentId: file.getParents()) {
             traceParentRefs(parentId, "", folders, fileId);
         }
         Collections.sort(folders);
-        String foldersStr = TextUtils.join(", ", folders);
-        PasswdSafeUtil.dbginfo(TAG, "compFolders %s: %s",
-                               remfile.getName(), foldersStr);
-        return foldersStr;
+        return TextUtils.join(", ", folders);
     }
 
 
@@ -151,8 +79,8 @@ public class FileFolders
     {
         File file = itsFileCache.get(id);
         if (file == null) {
-            file = itsDrive.files().get(id).setFields(
-                    GDriveProvider.FILE_FIELDS).execute();
+            file = itsDrive.files().get(id)
+                           .setFields(GDriveProvider.FILE_FIELDS).execute();
             itsFileCache.put(id, file);
         }
         return file;
