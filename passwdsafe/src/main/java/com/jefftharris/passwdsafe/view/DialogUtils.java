a94040f2d77e42261e145b279b63887470d780eb
Jeff Harris <jefftharris@users.noreply.github.com>
2016-05-30 21:47:52 -0400

    Replace last use of DialogUtils.createConfirmPrompt with ConfirmPromptDialog fragment

--- passwdsafe/src/main/java/com/jefftharris/passwdsafe/view/DialogUtils.java
+++ passwdsafe/src/main/java/com/jefftharris/passwdsafe/view/DialogUtils.java
@@ -7,22 +7,11 @@
  */
 package com.jefftharris.passwdsafe.view;
 
-import com.jefftharris.passwdsafe.R;
-import com.jefftharris.passwdsafe.lib.view.AbstractDialogClickListener;
 import com.jefftharris.passwdsafe.lib.view.GuiUtils;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.TextView;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 /**
  * The DialogUtils class provides utility dialogs
@@ -52,84 +41,5 @@ public class DialogUtils
                 }
             }
         });
-    }
-
-    /**
-     * The DialogData class encapsulates the data returned from a dialog
-     * creation method
-     */
-    public static class DialogData
-    {
-        public final Dialog itsDialog;
-        public final DialogValidator itsValidator;
-
-        public DialogData(Dialog dlg, DialogValidator validator)
-        {
-            itsDialog = dlg;
-            itsValidator = validator;
-        }
-
-    }
-
-    /**
-     * Create a dialog used as a prompt to confirm an operation
-     */
-    // TODO: fix generic confirm prompt for new dialog
-    public static DialogData createConfirmPrompt
-    (
-        Activity act,
-        AbstractDialogClickListener dlgClick,
-        String titleStr,
-        String promptStr
-    )
-    {
-        LayoutInflater factory = LayoutInflater.from(act);
-        @SuppressLint("InflateParams")
-        View dlgView = factory.inflate(R.layout.confirm_prompt, null);
-
-        AlertDialog.Builder alert = new AlertDialog.Builder(act)
-            .setTitle(titleStr)
-            .setMessage(promptStr)
-            .setView(dlgView)
-            .setPositiveButton(R.string.ok, dlgClick)
-            .setNegativeButton(R.string.cancel, dlgClick)
-            .setOnCancelListener(dlgClick);
-        AlertDialog alertDialog = alert.create();
-
-        final DialogValidator validator =
-            new DialogValidator.AlertValidator(alertDialog, dlgView, act)
-        {
-            @Override
-            public final void reset()
-            {
-                CheckBox confirmCb =
-                    (CheckBox)getDialog().findViewById(R.id.confirm);
-                confirmCb.setChecked(false);
-                super.reset();
-            }
-
-            @Override
-            protected final String doValidation()
-            {
-                CheckBox confirmCb =
-                    (CheckBox)getDialog().findViewById(R.id.confirm);
-                if (!confirmCb.isChecked()) {
-                    return getString(R.string.check_box_to_confirm);
-                }
-                return super.doValidation();
-            }
-        };
-
-        CheckBox confirmCb = (CheckBox)dlgView.findViewById(R.id.confirm);
-        confirmCb.setOnCheckedChangeListener(new OnCheckedChangeListener()
-        {
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked)
-            {
-                validator.validate();
-            }
-        });
-
-        return new DialogData(alertDialog, validator);
     }
 }
