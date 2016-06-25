9dc2e5538e042fdddb7620c9462806233fac673a
Jeff Harris <jefftharris@users.noreply.github.com>
2016-06-25 12:47:24 -0400

    Initialize SyncDb from content provider

--- sync/src/main/java/com/jefftharris/passwdsafe/sync/PasswdSafeProvider.java
+++ sync/src/main/java/com/jefftharris/passwdsafe/sync/PasswdSafeProvider.java
@@ -1,7 +1,8 @@
 /*
- * Copyright (©) 2013-2014 Jeff Harris <jefftharris@gmail.com> All rights reserved.
- * Use of the code is allowed under the Artistic License 2.0 terms, as specified
- * in the LICENSE file distributed with this code, or available from
+ * Copyright (©) 2016 Jeff Harris <jefftharris@gmail.com>
+ * All rights reserved. Use of the code is allowed under the
+ * Artistic License 2.0 terms, as specified in the LICENSE file
+ * distributed with this code, or available from
  * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 package com.jefftharris.passwdsafe.sync;
@@ -312,6 +313,8 @@ public class PasswdSafeProvider extends ContentProvider
     public boolean onCreate()
     {
         PasswdSafeUtil.dbginfo(TAG, "onCreate");
+        //noinspection ConstantConditions
+        SyncDb.initializeDb(getContext().getApplicationContext());
         itsListener = new OnAccountsUpdateListener()
         {
             @Override
