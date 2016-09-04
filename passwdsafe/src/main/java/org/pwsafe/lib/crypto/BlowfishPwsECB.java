60311678e6df00aa31cd49e6fb191fa359a395e3
Jeff Harris <jefftharris@gmail.com>
2016-09-03 22:36:55 -0400

    Reformat

--- passwdsafe/src/main/java/org/pwsafe/lib/crypto/BlowfishPwsECB.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/crypto/BlowfishPwsECB.java
@@ -12,52 +12,55 @@ package org.pwsafe.lib.crypto;
 import org.pwsafe.lib.Util;
 import org.pwsafe.lib.exception.PasswordSafeException;
 
-public class BlowfishPwsECB extends BlowfishPws {
-	/**
-	 * Constructor, sets the initial vector to zero.
-	 * 
-	 * @param bfkey the encryption/decryption key.
-	 */
-	public BlowfishPwsECB( byte[] bfkey )
-	{
-		super(bfkey, zeroIV(), false);
-	}
+public class BlowfishPwsECB extends BlowfishPws
+{
+    /**
+     * Constructor, sets the initial vector to zero.
+     *
+     * @param bfkey the encryption/decryption key.
+     */
+    public BlowfishPwsECB(byte[] bfkey)
+    {
+        super(bfkey, zeroIV(), false);
+    }
 
-	/**
-	 * Decrypts <code>buffer</code> in place.
-	 * 
-	 * @param buffer the buffer to be decrypted.
-	 * @throws PasswordSafeException 
+    /**
+     * Decrypts <code>buffer</code> in place.
+     *
+     * @param buffer the buffer to be decrypted.
+     * @throws PasswordSafeException
+     */
+    public void decrypt(byte[] buffer) throws PasswordSafeException
+    {
+        /* The endian conversion is simply to make this compatible with
+	 * use in previous versions of PasswordSafe (in ECB mode).  Why
+	 * the inversion is necessary for CBC mode and why it has to
+	 * "cancelled out" in this (ECB mode), I don't know but it
+	 * is the only way to get the correct ordering for the
+	 * CBC and ECB contexts within a standard password safe file.
 	 */
-	public void decrypt( byte[] buffer ) throws PasswordSafeException {
-		/* The endian conversion is simply to make this compatible with
-		 * use in previous versions of PasswordSafe (in ECB mode).  Why
-		 * the inversion is necessary for CBC mode and why it has to
-		 * "cancelled out" in this (ECB mode), I don't know but it
-		 * is the only way to get the correct ordering for the
-		 * CBC and ECB contexts within a standard password safe file.
-		 */
-		Util.bytesToLittleEndian(buffer);
-		super.decrypt(buffer);
-		Util.bytesToLittleEndian(buffer);
-	}
+        Util.bytesToLittleEndian(buffer);
+        super.decrypt(buffer);
+        Util.bytesToLittleEndian(buffer);
+    }
 
-	/**
-	 * Encrypts <code>buffer</code> in place.
-	 * 
-	 * @param buffer the buffer to be encrypted.
-	 * @throws PasswordSafeException 
+    /**
+     * Encrypts <code>buffer</code> in place.
+     *
+     * @param buffer the buffer to be encrypted.
+     * @throws PasswordSafeException
+     */
+    public void encrypt(byte[] buffer) throws PasswordSafeException
+    {
+	/* The endian conversion is simply to make this compatible with
+	 * use in previous versions of PasswordSafe (in ECB mode).  Why
+	 * the inversion is necessary for CBC mode and why it has to
+	 * "cancelled out" in this (ECB mode), I don't know but it
+	 * is the only way to get the correct ordering for the
+	 * CBC and ECB contexts within a standard password safe file.
 	 */
-	public void encrypt( byte[] buffer ) throws PasswordSafeException {
-		/* The endian conversion is simply to make this compatible with
-		 * use in previous versions of PasswordSafe (in ECB mode).  Why
-		 * the inversion is necessary for CBC mode and why it has to
-		 * "cancelled out" in this (ECB mode), I don't know but it
-		 * is the only way to get the correct ordering for the
-		 * CBC and ECB contexts within a standard password safe file.
-		 */
-		Util.bytesToLittleEndian(buffer);
-		super.encrypt(buffer);
-		Util.bytesToLittleEndian(buffer);
-	}
+        Util.bytesToLittleEndian(buffer);
+        super.encrypt(buffer);
+        Util.bytesToLittleEndian(buffer);
+    }
 }
