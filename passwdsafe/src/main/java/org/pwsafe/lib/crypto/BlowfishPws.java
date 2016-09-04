a1e518788de421d1253aba52e1c5785a9a4bd5be
Jeff Harris <jefftharris@gmail.com>
2016-09-03 22:30:49 -0400

    Reformat

--- passwdsafe/src/main/java/org/pwsafe/lib/crypto/BlowfishPws.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/crypto/BlowfishPws.java
@@ -9,8 +9,6 @@
  */
 package org.pwsafe.lib.crypto;
 
-import java.nio.ByteBuffer;
-
 import org.bouncycastle.crypto.BlockCipher;
 import org.bouncycastle.crypto.engines.BlowfishEngine;
 import org.bouncycastle.crypto.modes.CBCBlockCipher;
@@ -19,116 +17,128 @@ import org.bouncycastle.crypto.params.ParametersWithIV;
 import org.pwsafe.lib.Util;
 import org.pwsafe.lib.exception.PasswordSafeException;
 
+import java.nio.ByteBuffer;
+
 /**
  * A reimplementation of the BlowfishPws class to use the Bouncy Castle
  * implementation of Blowfish.
- * 
+ *
  * @author Michael Tiller
  */
 public class BlowfishPws
-{ 
-	private final BlockCipher decipher;
-	private final BlockCipher encipher;
+{
+    private final BlockCipher decipher;
+    private final BlockCipher encipher;
 
-	/**
-	 * Constructor, sets the initial vector to the value given.
-	 * 
-	 * @param bfkey      the encryption/decryption key.
-	 * @param ivBytes the initial vector.
-	 */
-	public BlowfishPws( byte[] bfkey, byte[] ivBytes ) {
-		this(bfkey, ivBytes, true);
-	}
+    /**
+     * Constructor, sets the initial vector to the value given.
+     *
+     * @param bfkey   the encryption/decryption key.
+     * @param ivBytes the initial vector.
+     */
+    public BlowfishPws(byte[] bfkey, byte[] ivBytes)
+    {
+        this(bfkey, ivBytes, true);
+    }
 
-	/**
-	 * Constructor, sets the initial vector to the value given.
-	 * 
-	 * @param bfkey      the encryption/decryption key.
-	 * @param ivBytes the initial vector.
-	 * @param cbc Use CBC mode (otherwise ECB is used).  Normally this should be true.
-	 */
-	BlowfishPws( byte[] bfkey, byte[] ivBytes, boolean cbc )
-	{
-		byte[] riv = Util.cloneByteArray(ivBytes);
-		Util.bytesToLittleEndian(riv);
-		KeyParameter dkp;
-		KeyParameter ekp;
-		if (cbc) {
-			decipher = new CBCBlockCipher(new BlowfishEngine());
-			encipher = new CBCBlockCipher(new BlowfishEngine());
-	    	dkp = new KeyParameter(bfkey);
-	    	ParametersWithIV div = new ParametersWithIV(dkp, riv);
-	    	ekp = new KeyParameter(bfkey);
-	    	ParametersWithIV eiv = new ParametersWithIV(ekp, riv);
-			decipher.init(false, div);
-			encipher.init(true, eiv);
-		} else {
-			/* ECB mode */
-			decipher = new BlowfishEngine();
-			encipher = new BlowfishEngine();
-	    	dkp = new KeyParameter(bfkey);
-	    	ekp = new KeyParameter(bfkey);
-			decipher.init(false, dkp);
-			encipher.init(true, ekp);
-		}
-	}
+    /**
+     * Constructor, sets the initial vector to the value given.
+     *
+     * @param bfkey   the encryption/decryption key.
+     * @param ivBytes the initial vector.
+     * @param cbc     Use CBC mode (otherwise ECB is used).  Normally this
+     *                       should be true.
+     */
+    BlowfishPws(byte[] bfkey, byte[] ivBytes, boolean cbc)
+    {
+        byte[] riv = Util.cloneByteArray(ivBytes);
+        Util.bytesToLittleEndian(riv);
+        KeyParameter dkp;
+        KeyParameter ekp;
+        if (cbc) {
+            decipher = new CBCBlockCipher(new BlowfishEngine());
+            encipher = new CBCBlockCipher(new BlowfishEngine());
+            dkp = new KeyParameter(bfkey);
+            ParametersWithIV div = new ParametersWithIV(dkp, riv);
+            ekp = new KeyParameter(bfkey);
+            ParametersWithIV eiv = new ParametersWithIV(ekp, riv);
+            decipher.init(false, div);
+            encipher.init(true, eiv);
+        } else {
+                        /* ECB mode */
+            decipher = new BlowfishEngine();
+            encipher = new BlowfishEngine();
+            dkp = new KeyParameter(bfkey);
+            ekp = new KeyParameter(bfkey);
+            decipher.init(false, dkp);
+            encipher.init(true, ekp);
+        }
+    }
 
-	/**
-	 * Decrypts <code>buffer</code> in place.
-	 * 
-	 * @param buffer the buffer to be decrypted.
-	 * @throws PasswordSafeException 
-	 */
-	public void decrypt( byte[] buffer ) throws PasswordSafeException
-	{
-		int bs = decipher.getBlockSize();
-		byte[] temp = new byte[buffer.length];
-		Util.bytesToLittleEndian( buffer );
+    /**
+     * Decrypts <code>buffer</code> in place.
+     *
+     * @param buffer the buffer to be decrypted.
+     * @throws PasswordSafeException
+     */
+    public void decrypt(byte[] buffer) throws PasswordSafeException
+    {
+        int bs = decipher.getBlockSize();
+        byte[] temp = new byte[buffer.length];
+        Util.bytesToLittleEndian(buffer);
 
-		if ((buffer.length % bs)!=0) {
-			throw new PasswordSafeException("Block size must be a multiple of cipher block size ("+bs+")");
-		}
-		for(int i=0;i<buffer.length;i+=bs) {
-			decipher.processBlock(buffer, i, temp, i);
-		}
-		
-		Util.copyBytes(temp, buffer);
-		Util.bytesToLittleEndian( buffer );
-	}
+        if ((buffer.length % bs) != 0) {
+            throw new PasswordSafeException(
+                    "Block size must be a multiple of cipher block size (" +
+                    bs + ")");
+        }
+        for (int i = 0; i < buffer.length; i += bs) {
+            decipher.processBlock(buffer, i, temp, i);
+        }
 
-	/**
-	 * Encrypts <code>buffer</code> in place.
-	 * 
-	 * @param buffer the buffer to be encrypted.
-	 * @throws PasswordSafeException 
-	 */
-	public void encrypt( byte[] buffer ) throws PasswordSafeException
-	{
-		int bs = encipher.getBlockSize();
-		byte[] temp = new byte[buffer.length];
-		Util.bytesToLittleEndian( buffer );
+        Util.copyBytes(temp, buffer);
+        Util.bytesToLittleEndian(buffer);
+    }
 
-		if ((buffer.length % bs)!=0) {
-			throw new PasswordSafeException("Block size must be a multiple of cipher block size ("+bs+")");
-		}
-		for(int i=0;i<buffer.length;i+=bs) {
-			encipher.processBlock(buffer, i, temp, i);
-		}
+    /**
+     * Encrypts <code>buffer</code> in place.
+     *
+     * @param buffer the buffer to be encrypted.
+     * @throws PasswordSafeException
+     */
+    public void encrypt(byte[] buffer) throws PasswordSafeException
+    {
+        int bs = encipher.getBlockSize();
+        byte[] temp = new byte[buffer.length];
+        Util.bytesToLittleEndian(buffer);
 
-		Util.bytesToLittleEndian( temp );
-		Util.copyBytes(temp, buffer);
-	}
+        if ((buffer.length % bs) != 0) {
+            throw new PasswordSafeException(
+                    "Block size must be a multiple of cipher block size (" +
+                    bs + ")");
+        }
+        for (int i = 0; i < buffer.length; i += bs) {
+            encipher.processBlock(buffer, i, temp, i);
+        }
 
-	protected static byte[] zeroIV() {
-		byte[] ret = new byte[8];
-		for(int i=0;i<8;i++) { ret[i] = 0; }
-		return ret;
-	}
-	
-	public static byte[] makeByteKey(long key) {
-		byte ivBytes[] = new byte[8];
-		ByteBuffer buf = ByteBuffer.wrap(ivBytes);  
-		buf.putLong(key);
-		return ivBytes;
-	}
+        Util.bytesToLittleEndian(temp);
+        Util.copyBytes(temp, buffer);
+    }
+
+    protected static byte[] zeroIV()
+    {
+        byte[] ret = new byte[8];
+        for (int i = 0; i < 8; i++) {
+            ret[i] = 0;
+        }
+        return ret;
+    }
+
+    public static byte[] makeByteKey(long key)
+    {
+        byte ivBytes[] = new byte[8];
+        ByteBuffer buf = ByteBuffer.wrap(ivBytes);
+        buf.putLong(key);
+        return ivBytes;
+    }
 }
