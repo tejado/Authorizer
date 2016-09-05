daca2e01563187f5d7be032ba5ada9ddd8ce76ab
Jeff Harris <jefftharris@users.noreply.github.com>
2016-09-04 22:28:01 -0400

    Cleanup warnings

--- passwdsafe/src/main/java/org/pwsafe/lib/exception/EndOfFileException.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/exception/EndOfFileException.java
@@ -14,7 +14,6 @@ package org.pwsafe.lib.exception;
  * 
  * @author Kevin Preece 
  */
-@SuppressWarnings("ALL")
 public class EndOfFileException extends Exception
 {
 
@@ -28,31 +27,6 @@ public class EndOfFileException extends Exception
 	 */
 	public EndOfFileException()
 	{
-		super();
 	}
 
-	/**
-	 * @param arg0
-	 */
-	public EndOfFileException(String arg0)
-	{
-		super(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 */
-	public EndOfFileException(Throwable arg0)
-	{
-		super(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 */
-	public EndOfFileException(String arg0, Throwable arg1)
-	{
-		super(arg0, arg1);
-	}
 }
