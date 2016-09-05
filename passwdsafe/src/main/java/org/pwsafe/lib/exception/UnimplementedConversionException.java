ad5a0e70d88f9ce462c22c1147d1c64a2e438170
Jeff Harris <jefftharris@gmail.com>
2016-09-04 22:48:22 -0400

    Cleanup warnings and reformat

--- passwdsafe/src/main/java/org/pwsafe/lib/exception/UnimplementedConversionException.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/exception/UnimplementedConversionException.java
@@ -11,48 +11,17 @@ package org.pwsafe.lib.exception;
 
 /**
  * An exception thrown when a required conversion has not been implemented.
- * 
+ *
  * @author Kevin Preece
  */
-@SuppressWarnings("ALL")
 public class UnimplementedConversionException extends RuntimeException
 {
-
-	/**
-     * 
-     */
     private static final long serialVersionUID = 1L;
 
     /**
-	 * 
-	 */
-	public UnimplementedConversionException()
-	{
-		super();
-	}
-
-	/**
-	 * @param arg0
-	 */
-	public UnimplementedConversionException(String arg0)
-	{
-		super(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 */
-	public UnimplementedConversionException(Throwable arg0)
-	{
-		super(arg0);
-	}
-
-	/**
-	 * @param arg0
-	 * @param arg1
-	 */
-	public UnimplementedConversionException(String arg0, Throwable arg1)
-	{
-		super(arg0, arg1);
-	}
+     * Constructor
+     */
+    public UnimplementedConversionException()
+    {
+    }
 }
