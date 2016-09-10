c40abb32865c2a877efe5c0479b01a7bc2c84250
Jeff Harris <jefftharris@gmail.com>
2016-09-09 22:08:15 -0400

    Cleanup warnings and reformat

--- passwdsafe/src/main/java/org/pwsafe/lib/file/PwsStringUnicodeField.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/file/PwsStringUnicodeField.java
@@ -1,6 +1,4 @@
 /*
- * $Id: PwsStringField.java 294 2004-02-22 15:29:03Z preecek $
- *
  * Copyright (c) 2008-2009 David Muller <roxon@users.sourceforge.net>.
  * All rights reserved. Use of the code is allowed under the
  * Artistic License 2.0 terms, as specified in the LICENSE file
@@ -16,133 +14,97 @@ import java.io.UnsupportedEncodingException;
 /**
  * @author Kevin Preece
  */
-@SuppressWarnings("ALL")
 public class PwsStringUnicodeField extends PwsField
 {
-	private static final long serialVersionUID = -4530429748953931053L;
+    private static final long serialVersionUID = -4530429748953931053L;
 
-	/**
-	 * Constructor
-	 *
-	 * @param type  the field's type.
-	 * @param value the field's value.
-	 * @throws UnsupportedEncodingException
-	 */
-	public PwsStringUnicodeField( int type, byte[] value ) throws UnsupportedEncodingException
-	{
+    /**
+     * Constructor
+     *
+     * @param type  the field's type.
+     * @param value the field's value.
+     * @throws UnsupportedEncodingException
+     */
+    public PwsStringUnicodeField(int type, byte[] value)
+            throws UnsupportedEncodingException
+    {
 
-		super( type, new String(value, "UTF-8" ));
-	}
+        super(type, new String(value, "UTF-8"));
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param type  the field's type.
-	 * @param value the field's value.
-	 */
-	public PwsStringUnicodeField( int type, String value ) {
-		super( type, value);
-	}
+    /**
+     * Constructor
+     *
+     * @param type  the field's type.
+     * @param value the field's value.
+     */
+    public PwsStringUnicodeField(int type, String value)
+    {
+        super(type, value);
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param type  the field's type.
-	 * @param value the field's value.
-	 */
-	public PwsStringUnicodeField( int type, StringBuilder value ) {
-		// TODOlib: allow StringBuilder or CharSequence as value further up.
-		super( type, value != null ? value.toString() : null);
-	}
+    /**
+     * Constructor
+     *
+     * @param type  the field's type.
+     * @param value the field's value.
+     */
+    public PwsStringUnicodeField(PwsFieldType type, String value)
+    {
+        super(type, value);
+    }
 
-	/**
-	 * Constructor
-	 *
-	 * @param type  the field's type.
-	 * @param value the field's value.
-	 */
-	public PwsStringUnicodeField( PwsFieldType type, String value ) {
-		super( type, value);
-	}
+    /**
+     * Returns the field's value as a byte array.
+     *
+     * @return A byte array containing the field's data.
+     * @see org.pwsafe.lib.file.PwsField#getBytes()
+     */
+    @Override
+    public byte[] getBytes()
+    {
+        try {
+            return ((String)super.getValue()).getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * Returns the field's value as a byte array.
-	 *
-	 * @return A byte array containing the field's data.
-	 *
-	 * @see org.pwsafe.lib.file.PwsField#getBytes()
-	 */
-	@Override
-	public byte[] getBytes()
-	{
-		try {
-			return ((String) super.getValue()).getBytes("UTF-8");
-		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * Compares this <code>PwsStringField</code> to another returning a value
+     * less than zero if <code>this</code> is "less than" <code>that</code>,
+     * zero if they're equal and greater than zero if <code>this</code>
+     * is "greater than" <code>that</code>.
+     *
+     * @param that the other field to compare to.
+     * @return A value less than zero if <code>this</code> is "less than"
+     * <code>that</code>, zero if they're equal and greater than zero if
+     * <code>this</code> is "greater than" <code>that</code>.
+     */
+    public int compareTo(@NonNull Object that)
+    {
+        return ((String)this.getValue()).compareTo(
+                (String)((PwsStringUnicodeField)that).getValue());
+    }
 
-	/**
-	 * Compares this <code>PwsStringField</code> to another returning a value less than zero if
-	 * <code>this</code> is "less than" <code>that</code>, zero if they're equal and greater
-	 * than zero if <code>this</code> is "greater than" <code>that</code>.
-	 *
-	 * @param that the other field to compare to.
-	 *
-	 * @return A value less than zero if <code>this</code> is "less than" <code>that</code>,
-	 *         zero if they're equal and greater than zero if <code>this</code> is "greater
-	 *         than" <code>that</code>.
-	 */
-	public int compareTo( @NonNull Object that )
-	{
-		return ((String) this.getValue()).compareTo((String) ((PwsStringUnicodeField) that).getValue());
-	}
-
-	/**
-	 * Compares this object to another <code>PwsStringField</code> or <code>String</code> returning
-	 * <code>true</code> if they're equal or <code>false</code> otherwise.
-	 *
-	 * @param arg0 the other object to compare to.
-	 *
-	 * @return <code>true</code> if they're equal or <code>false</code> otherwise.
-	 */
-	@Override
-	public boolean equals( Object arg0 )
-	{
-		if ( arg0 instanceof PwsStringUnicodeField )
-		{
-			return equals( (PwsStringUnicodeField) arg0 );
-		}
-		else if ( arg0 instanceof String )
-		{
-			return equals( (String) arg0 );
-		}
-		throw new ClassCastException();
-	}
-
-	/**
-	 * Compares this object to another <code>PwsStringField</code> returning
-	 * <code>true</code> if they're equal or <code>false</code> otherwise.
-	 *
-	 * @param arg0 the other object to compare to.
-	 *
-	 * @return <code>true</code> if they're equal or <code>false</code> otherwise.
-	 */
-	public boolean equals( PwsStringUnicodeField arg0 )
-	{
-		return super.getValue().equals(arg0.getValue());
-	}
-
-	/**
-	 * Compares this object to a <code>String</code> returning <code>true</code> if they're equal
-	 * or <code>false</code> otherwise.
-	 *
-	 * @param arg0 the other object to compare to.
-	 *
-	 * @return <code>true</code> if they're equal or <code>false</code> otherwise.
-	 */
-	public boolean equals( String arg0 )
-	{
-		return super.getValue().equals(arg0);
-	}
+    /**
+     * Compares this object to another <code>PwsStringField</code> or
+     * <code>String</code> returning <code>true</code> if they're equal or
+     * <code>false</code> otherwise.
+     *
+     * @param arg0 the other object to compare to.
+     * @return <code>true</code> if they're equal or <code>false</code>
+     * otherwise.
+     */
+    @Override
+    public boolean equals(Object arg0)
+    {
+        if (arg0 instanceof PwsStringUnicodeField) {
+            return super.getValue().equals(
+                    ((PwsStringUnicodeField)arg0).getValue());
+        } else if (arg0 instanceof String) {
+            return super.getValue().equals(arg0);
+        }
+        throw new ClassCastException();
+    }
 }
--- passwdsafe/src/main/java/org/pwsafe/lib/file/PwsUnknownField.java
+++ passwdsafe/src/main/java/org/pwsafe/lib/file/PwsUnknownField.java
@@ -14,94 +14,79 @@ import org.pwsafe.lib.Util;
 /**
  * Bit bucket for fields we are not interested in processing but must
  * preserve in the output.
- * 
+ *
  * @author Glen Smith (based on the work of Kevin Preece)
  */
-@SuppressWarnings("ALL")
 public class PwsUnknownField extends PwsField
 {
-	/**
-     * 
-     */
     private static final long serialVersionUID = 1L;
 
     /**
-	 * Constructor
-	 * 
-	 * @param type  the field's type.
-	 * @param value the field's value.
-	 */
-	public PwsUnknownField( int type, byte [] value )
-	{
-		super( type, value );
-	}
+     * Constructor
+     *
+     * @param type  the field's type.
+     * @param value the field's value.
+     */
+    public PwsUnknownField(int type, byte[] value)
+    {
+        super(type, value);
+    }
 
 
-	/**
-	 * Returns the field's value as a byte array.
-	 * 
-	 * @return A byte array containing the field's data.
-	 * 
-	 * @see org.pwsafe.lib.file.PwsField#getBytes()
-	 */
-	@Override
-	public byte[] getBytes()
-	{
-		return ((byte[]) super.getValue());
-	}
+    /**
+     * Returns the field's value as a byte array.
+     *
+     * @return A byte array containing the field's data.
+     * @see org.pwsafe.lib.file.PwsField#getBytes()
+     */
+    @Override
+    public byte[] getBytes()
+    {
+        return ((byte[])super.getValue());
+    }
 
-	/**
-	 * Compares this <code>PwsUUIDField</code> to another returning a value less than zero if
-	 * <code>this</code> is "less than" <code>that</code>, zero if they're equal and greater
-	 * than zero if <code>this</code> is "greater than" <code>that</code>.
-	 * 
-	 * @param that the other field to compare to. 
-	 * 
-	 * @return A value less than zero if <code>this</code> is "less than" <code>that</code>,
-	 *         zero if they're equal and greater than zero if <code>this</code> is "greater
-	 *         than" <code>that</code>.
-	 */
-	public int compareTo( @NonNull Object that )
-	{
-		byte[] thisB = (byte[]) this.getValue();
-		byte[] thatB = (byte[]) ((PwsUnknownField) that).getValue();
-		return thisB[0] > thatB[0] ? 1 : 0;
-	}
+    /**
+     * Compares this <code>PwsUUIDField</code> to another returning a
+     * value less than zero if <code>this</code> is "less than"
+     * <code>that</code>, zero if they're equal and greater than zero if
+     * <code>this</code> is "greater than" <code>that</code>.
+     *
+     * @param that the other field to compare to.
+     * @return A value less than zero if <code>this</code> is "less than"
+     * <code>that</code>, zero if they're equal and greater than zero if
+     * <code>this</code> is "greater than" <code>that</code>.
+     */
+    public int compareTo(@NonNull Object that)
+    {
+        byte[] thisB = (byte[])this.getValue();
+        byte[] thatB = (byte[])((PwsUnknownField)that).getValue();
+        return thisB[0] > thatB[0] ? 1 : 0;
+    }
 
-	/**
-	 * Compares this object to another <code>PwsUUIDField</code> or UUID returning
-	 * <code>true</code> if they're equal or <code>false</code> otherwise.
-	 * 
-	 * @param arg0 the other object to compare to.
-	 * 
-	 * @return <code>true</code> if they're equal or <code>false</code> otherwise.
-	 */
-	@Override
-	public boolean equals( Object arg0 )
-	{
-		if ( arg0 instanceof PwsUnknownField )
-		{
-			return equals( (PwsUnknownField) arg0 );
-		}
-		throw new ClassCastException();
-	}
+    /**
+     * Compares this object to another <code>PwsUUIDField</code> or UUID
+     * returning <code>true</code> if they're equal or <code>false</code>
+     * otherwise.
+     *
+     * @param arg0 the other object to compare to.
+     * @return <code>true</code> if they're equal or <code>false</code>
+     * otherwise.
+     */
+    @Override
+    public boolean equals(Object arg0)
+    {
+        if (arg0 instanceof PwsUnknownField) {
+            return Util.bytesAreEqual(
+                    (byte[])getValue(),
+                    (byte[])((PwsUnknownField)arg0).getValue());
+        }
+        throw new ClassCastException();
+    }
 
-	/**
-	 * Compares this object to another <code>PwsUnknownField</code> returning
-	 * <code>true</code> if they're equal or <code>false</code> otherwise.
-	 * 
-	 * @param other the other object to compare to.
-	 * 
-	 * @return <code>true</code> if they're equal or <code>false</code> otherwise.
-	 */
-	public boolean equals( PwsUnknownField other )
-	{
-		return Util.bytesAreEqual((byte[])getValue(), (byte[])other.getValue());
-	}
-	
-	@Override
-	public String toString() {
-		return new String(((byte[]) super.getValue()));
-	}
+    @Override
+    public String toString()
+    {
+        return new String(((byte[])super.getValue()));
+    }
 
 }
